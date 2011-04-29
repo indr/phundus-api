874b58aa03c0e759255d3727ee675eea88a8545f
Inder <Inder@0fb50134-772a-4728-8726-cc0728632084>
2011-04-29 09:13:12 +0000

    Interface für MembershipService hinzugefügt.

---
+++ trunk/src/phiNdus.fundus.Core.Business/IMembershipService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace phiNdus.fundus.Core.Business
+{
+    interface IMembershipService
+    {
+        UserDto CreateUser(Guid sessionKey, string email);
+        UserDto GetUser(Guid sessionKey, string name, string password);
+        UserDto UpdateUser(Guid sessionKey, UserDto user);
+        UserDto ChangePassword(Guid sessionKey, long userId);
+        void DeleteUser(Guid sessionKey, long userId);
+        bool ValidateUser(Guid sessionKey, string username, string password);
+    }
+}
