0d6335f4ff186467939884ef64d1d8faad6a1d42
Reto Inderbitzin <mail@indr.ch>
2011-05-11 14:05:54 +0000

    NHibernate fängt beim Versionsattribut mit 1 an. So tun wir!

--- sql/dml-integration.sql
+++ sql/dml-integration.sql
@@ -4,17 +4,16 @@ SET ANSI_NULLS ON;
 
 SET QUOTED_IDENTIFIER ON;
 
-INSERT INTO [User] (Id, Version, FirstName, LastName) VALUES (1, 0, 'Ted', 'Mosby');
-INSERT INTO [User] (Id, Version, FirstName, LastName) VALUES (2, 0, 'Marshall', 'Eriksen');
-INSERT INTO [User] (Id, Version, FirstName, LastName) VALUES (3, 0, 'Robin', 'Scherbatsky');
-INSERT INTO [User] (Id, Version, FirstName, LastName) VALUES (4, 0, 'Barney', 'Stinson');
+INSERT INTO [User] (Id, Version, FirstName, LastName) VALUES (1, 1, 'Ted', 'Mosby');
+INSERT INTO [User] (Id, Version, FirstName, LastName) VALUES (2, 1, 'Marshall', 'Eriksen');
+INSERT INTO [User] (Id, Version, FirstName, LastName) VALUES (3, 1, 'Robin', 'Scherbatsky');
+INSERT INTO [User] (Id, Version, FirstName, LastName) VALUES (4, 1, 'Barney', 'Stinson');
 
 INSERT INTO [Membership] (Id, Version, Password, Email, IsApproved, IsLockedOut, CreateDate)
-  VALUES (1, 0, NULL, 'ted.mosby@example.com', 0, 0, '2011-05-10');
+  VALUES (1, 1, NULL, 'ted.mosby@example.com', 0, 0, '2011-05-10');
 INSERT INTO [Membership] (Id, Version, Password, Email, IsApproved, IsLockedOut, CreateDate)
-  VALUES (2, 0, NULL, 'marshall.eriksen@example.com', 0, 0, '2011-05-10');
+  VALUES (2, 1, NULL, 'marshall.eriksen@example.com', 0, 0, '2011-05-10');
 INSERT INTO [Membership] (Id, Version, Password, Email, IsApproved, IsLockedOut, CreateDate)
-  VALUES (3, 0, NULL, 'robin.scherbatsky@example.com', 0, 0, '2011-05-10');
+  VALUES (3, 1, NULL, 'robin.scherbatsky@example.com', 0, 0, '2011-05-10');
 INSERT INTO [Membership] (Id, Version, Password, Email, IsApproved, IsLockedOut, CreateDate)
-  VALUES (4, 0, NULL, 'barney.stinson@example.com', 0, 0, '2011-05-10');
-
+  VALUES (4, 1, NULL, 'barney.stinson@example.com', 0, 0, '2011-05-10');
