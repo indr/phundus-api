bebb1160a759acc0140eeadfc293033274f211c1
Reto Inderbitzin <mail@indr.ch>
2011-06-16 16:47:51 +0000

    AcceptanceTests-Projekt hinzugefügt.

---
+++ tests/phiNdus.fundus.AcceptanceTests/DummyTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace phiNdus.fundus.AcceptanceTests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        [Test]
+        public void DummyTest()
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}
