b8aea27f30321ac370d1827ae76169a9c5271e02
Inder <Inder@0fb50134-772a-4728-8726-cc0728632084>
2011-06-16 15:15:06 +0000

    phiNdus.fundus.SmokeTests-Projekt hinzugefügt. AssemblyInfo.cs-Dateien entfernt und ignoriert. Solution-Datei verschoben.

---
+++ trunk/tests/phiNdus.fundus.SmokeTests/DeploymentTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using NUnit.Framework;
+
+namespace phiNdus.fundus.SmokeTests
+{
+    [TestFixture]
+    public class DeploymentTests
+    {
+        [Test]
+        public void Can_get_home_page()
+        {
+            var request = WebRequest.Create("http://fundus.phindus.ch/staging");
+            request.Timeout = 1000;
+
+            var response = request.GetResponse();
+            Assert.That(response, Is.Not.Null);
+
+            var stream = new StreamReader(response.GetResponseStream());
+            var content = stream.ReadToEnd();
+
+            Assert.That(content, Contains.Substring("<h2>Home &gt; Index</h2>"));
+        }
+    }
+}
