c138599b458f4140567ef30e3db07d51781d3740
Reto Inderbitzin <mail@indr.ch>
2011-06-17 08:36:13 +0000

    WatiN hinzugefügt. Acceptance-Test mit automatisiertem GUI-Test. Wird mit Build Agent ziemlich sicher fehlschlagen. Sorry im Voraus für den kommenden Spam! Nichtsdestotrotz: Heute wird ein guter Tag! :o)

---
+++ tests/phiNdus.fundus.AcceptanceTests/AccountTests.cs
@@ -0,0 +1,53 @@
+﻿using Castle.Windsor;
+using NUnit.Framework;
+using phiNdus.fundus.Core.Business;
+using phiNdus.fundus.Core.Business.Services;
+using Rhino.Commons;
+using WatiN.Core;
+
+namespace phiNdus.fundus.AcceptanceTests
+{
+    [TestFixture]
+    public class AccountTests
+    {
+        [TestFixtureSetUp]
+        public void FixtureSetUp()
+        {
+            IoC.Initialize(new WindsorContainer());
+            IoC.Container.Install(new Installer());
+
+            var appSettings = new System.Configuration.AppSettingsReader();
+            BaseUri = appSettings.GetValue("uri", typeof(string)).ToString();
+        }
+
+        [TestFixtureTearDown]
+        public void FixtureTearDown()
+        {
+            IoC.Container.Dispose();
+        }
+
+        protected string BaseUri { get; private set; }
+
+        [Test]
+        public void SignUp()
+        {
+            // Given a user with email dave@example.com
+            // When sign up with dave@example.com
+            // Then shows that email is already taken
+            
+            var userService = new UserService();
+            if (userService.GetUser("dave@example.com") == null)
+                userService.CreateUser("dave@example.com", "password");
+
+            using (var browser = new IE(BaseUri))
+            {
+                browser.GoTo(BaseUri + "/Account/SignUp");
+                browser.TextField(Find.ById("EMail")).TypeText("dave@example.com");
+                browser.Button(Find.ByValue("Anmelden")).Click();
+
+                // TODO,Inder: Falsche Bedingung!
+                Assert.IsTrue(browser.ContainsText("Konnte Benutzer nicht erstellen"));
+            }
+        }
+    }
+}
