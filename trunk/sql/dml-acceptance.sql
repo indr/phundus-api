d6602e118db76c865ba00914e6611d2dbd290956
Inder <Inder@0fb50134-772a-4728-8726-cc0728632084>
2011-06-17 08:36:13 +0000

    WatiN hinzugefügt. Acceptance-Test mit automatisiertem GUI-Test. Wird mit Build Agent ziemlich sicher fehlschlagen. Sorry im Voraus für den kommenden Spam! Nichtsdestotrotz: Heute wird ein guter Tag! :o)

---
+++ trunk/sql/dml-acceptance.sql
@@ -0,0 +1,20 @@
+USE [${sql.database-name}];
+
+SET ANSI_NULLS ON;
+
+SET QUOTED_IDENTIFIER ON;
+
+insert into [Setting] (Id, Version, [Key], [StringValue])
+  values (1, 1, 'mail.smtp.host', 'mail.indr.ch');
+insert into [Setting] (Id, Version, [Key], [StringValue])
+  values (2, 1, 'mail.smtp.user-name', 'fundus-sys-test-1@indr.ch');
+insert into [Setting] (Id, Version, [Key], [StringValue])
+  values (3, 1, 'mail.smtp.password', 'phiNdus');
+insert into [Setting] (Id, Version, [Key], [StringValue])
+  values (4, 1, 'mail.smtp.from', 'fundus-sys-test-1@indr.ch');  
+  
+insert into [Setting] (Id, Version, [Key], [StringValue])
+  values (5, 1, 'mail.templates.user-account-validation.subject', '[fundus] User Account Validation');
+insert into [Setting] (Id, Version, [Key], [StringValue])
+  values (6, 1, 'mail.templates.user-account-validation.body', 'Hello [User.FirstName]\r\n\r\nPlease go to the following link in order to validate your account:\r\n[Link.UserAccountValidation]\r\n\r\nThanks');
+ 
