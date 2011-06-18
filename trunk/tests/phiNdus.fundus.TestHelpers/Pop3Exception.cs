8e5c0f6176f8f088431973a8419764bccb7d6345
Inder <Inder@0fb50134-772a-4728-8726-cc0728632084>
2011-06-18 16:39:36 +0000

    Konfigurationen und dessen Management geändert. Buildskript aufgefräumt. Help.txt hinzugefügt. Hilfetext noch nicht vollständig.

---
+++ trunk/tests/phiNdus.fundus.TestHelpers/Pop3Exception.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace phiNdus.fundus.TestHelpers
+{
+    public class Pop3Exception : ApplicationException
+    {
+        public Pop3Exception(string str)
+            : base(str)
+        {
+        }
+    }
+}
