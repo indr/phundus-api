b605b28b1776ba940b7a41cafbdca5ff487a7aef
Inder <Inder@0fb50134-772a-4728-8726-cc0728632084>
2011-06-19 09:39:22 +0000

    Refactoring Acceptance-Tests: DslTestCase mit Window Driver Pattern implementation.

--- trunk/tests/phiNdus.fundus.AcceptanceTests/AccountTests.cs
+++ trunk/tests/phiNdus.fundus.AcceptanceTests/AccountTests.cs
@@ -1,33 +1,13 @@
-﻿using Castle.Windsor;
-using NUnit.Framework;
-using phiNdus.fundus.Core.Business;
+﻿using NUnit.Framework;
+using phiNdus.fundus.AcceptanceTests.AppDriver.WindowDriver;
 using phiNdus.fundus.Core.Business.Services;
-using Rhino.Commons;
 using WatiN.Core;
 
 namespace phiNdus.fundus.AcceptanceTests
 {
     [TestFixture]
-    public class AccountTests
+    public class AccountTests : DslTestCase
     {
-        [TestFixtureSetUp]
-        public void FixtureSetUp()
-        {
-            IoC.Initialize(new WindsorContainer());
-            IoC.Container.Install(new Installer());
-
-            var appSettings = new System.Configuration.AppSettingsReader();
-            BaseUri = appSettings.GetValue("uri", typeof(string)).ToString();
-        }
-
-        [TestFixtureTearDown]
-        public void FixtureTearDown()
-        {
-            IoC.Container.Dispose();
-        }
-
-        protected string BaseUri { get; private set; }
-
         [Test]
         public void SignUp()
         {
@@ -36,7 +16,7 @@ namespace phiNdus.fundus.AcceptanceTests
             // Given a user with email dave@example.com
             // When I sign up with dave@example.com
             // Then I see that email is already taken
-            
+
             var userService = new UserService();
             if (userService.GetUser("dave@example.com") == null)
                 userService.CreateUser("dave@example.com", "password");
@@ -52,35 +32,32 @@ namespace phiNdus.fundus.AcceptanceTests
         }
 
         [Test]
-        public void SignUpShowsRequiredFields()
-        {
-            // When I sign up without filling fields
-            // Then I see which fields are required
-
-            using (var browser = new IE(BaseUri + "/Account/SignUp"))
-            {
-                browser.Button(Find.ByValue("Registrieren")).Click();
-
-                Assert.IsTrue(browser.ContainsText("The Vorname field is required."));
-                Assert.IsTrue(browser.ContainsText("The Nachname field is required."));
-                Assert.IsTrue(browser.ContainsText("The E-Mail-Adresse field is required."));
-            }
-            
-        }
-
-        [Test]
         public void SignUpShowsInvalidEmail()
         {
             // When I sign up with invalid email
-            // Then I see invalid email
+            // Then I see Ungültige E-Mail-Adresse
 
-            using (var browser = new IE(BaseUri + "/Account/SignUp"))
-            {
-                browser.TextField(Find.ById("Email")).TypeText("abc.com");
-                browser.Button(Find.ByValue("Registrieren")).Click();
+            var signUpWindow = new SignUpWindowDriver(Context);
+            signUpWindow.SpecifyEmail("abc.com");
+            signUpWindow.SignUp();
 
-                Assert.IsTrue(browser.ContainsText("Ungültige E-Mail-Adresse"));
-            }
+            signUpWindow.ContainsText("Ungültige E-Mail-Adresse");
+        }
+
+        [Test]
+        public void SignUpShowsRequiredFields()
+        {
+            // When I sign up
+            // Then I see The Vorname field is required.
+            //  and I see The Nachname field is required.
+            //  and I see The E-Mail-Adresse field is required.
+
+            var signUpWindow = new SignUpWindowDriver(Context);
+            signUpWindow.SignUp();
+
+            signUpWindow.ContainsText("The Vorname field is required.");
+            signUpWindow.ContainsText("The Nachname field is required.");
+            signUpWindow.ContainsText("The E-Mail-Adresse field is required.");
         }
     }
 }
