7d29482a3f19cb22f80121d11d15bb6878b6a43a
Inder <Inder@0fb50134-772a-4728-8726-cc0728632084>
2011-06-23 11:05:00 +0000

    Link/URI-Ersetzung beim Mailversand.

---
+++ trunk/src/phiNdus.fundus.Core.Business/Mails/BaseMail.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using phiNdus.fundus.Core.Domain.Settings;
+using Rhino.Commons;
+
+namespace phiNdus.fundus.Core.Business.Mails
+{
+    public class BaseMail
+    {
+        private readonly IDictionary<string, object> _dataContext = new Dictionary<string, object>();
+
+        protected BaseMail(IMailTemplateSettings settings)
+        {
+            Subject = settings.Subject;
+            Body = settings.Body;
+
+            _dataContext.Add("Link", new Links(Settings.Common.ServerUrl));
+        }
+
+        private class Links
+        {
+            private readonly string _serverUrl;
+
+            public Links(string serverUrl)
+            {
+                _serverUrl = serverUrl;
+            }
+
+            public string UserAccountValidation
+            {
+                get { return "http://" + _serverUrl + "/Account/Validation/?key=[Membership.ValidationKey]"; }
+            }
+        }
+
+        protected IDictionary<string, object> DataContext
+        {
+            get { return _dataContext; }
+        }
+
+        public string Subject { get; protected set; }
+        public string Body { get; protected set; }
+
+        private static string Signature
+        {
+            get
+            {
+                return
+                    @"
+
+--
+This is automatically generated message from fundus.
+-
+If you think it was sent incorrectly contact the administrator at admin@example.com.";
+            }
+        }
+
+        private string GenerateSubject()
+        {
+            return ReplacePlaceholders(Subject);
+        }
+
+        private string GenerateBody()
+        {
+            return ReplacePlaceholders(Body + Signature);
+        }
+
+        protected void Send(string recipients)
+        {
+            var gateway = IoC.Resolve<IMailGateway>();
+            gateway.Send(recipients, GenerateSubject(), GenerateBody());
+        }
+
+        private string GetValue(string key)
+        {
+            int idx = key.LastIndexOf('.');
+            if (idx == -1)
+                return null;
+
+            string group = key.Substring(0, idx);
+
+
+            object data = null;
+            if (!_dataContext.TryGetValue(group, out data))
+                return null;
+
+            string name = key.Substring(idx + 1);
+            PropertyInfo[] propertyInfos = data.GetType().GetProperties();
+
+            foreach (PropertyInfo each in propertyInfos.Where(each => each.Name == name))
+                return each.GetValue(data, null).ToString();
+            return null;
+        }
+
+
+        private string ReplacePlaceholders(string input)
+        {
+            return ReplacePlaceholders(input, 1);
+        }
+
+        private string ReplacePlaceholders(string input, int depth)
+        {
+            string result = input;
+            var regex = new Regex(@"\[([^\]]*)\]");
+            Match match = regex.Match(input);
+            while (match.Success)
+            {
+                string value = GetValue(match.Groups[1].Value);
+                if (value != null)
+                    result = result.Replace(match.Value, value);
+                match = match.NextMatch();
+            }
+            if (depth < 2)
+                return ReplacePlaceholders(result, depth + 1);
+            return result;
+        }
+    }
+}
