a85a7280e37f06dfa69a43e880e5503468456ce1
Inder <Inder@0fb50134-772a-4728-8726-cc0728632084>
2011-09-17 19:34:28 +0000

    PropertyException hinzugefügt.

---
+++ trunk/src/phiNdus.fundus.Core.Domain.UnitTests/PropertyExceptionTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace phiNdus.fundus.Core.Domain.UnitTests
+{
+    [TestFixture]
+    public class PropertyExceptionTests
+    {
+        [Test]
+        public void Can_create()
+        {
+            var sut = new PropertyException();
+            Assert.That(sut, Is.Not.Null);
+        }
+
+        [Test]
+        public void Is_derived_from_Exception()
+        {
+            var sut = new PropertyException();
+            Assert.That(sut, Is.InstanceOf(typeof (Exception)));
+        }
+    }
+}
---
+++ trunk/src/phiNdus.fundus.Core.Domain/PropertyException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Security;
+
+namespace phiNdus.fundus.Core.Domain
+{
+    public class PropertyException : Exception
+    {
+        public PropertyException()
+        {
+        }
+
+        public PropertyException(string message) : base(message)
+        {
+        }
+
+        public PropertyException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        [SecuritySafeCritical]
+        protected PropertyException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
