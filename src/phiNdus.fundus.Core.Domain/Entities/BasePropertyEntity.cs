f3956e8574ff1c22d2fbb9bff2bc83af4a1c775c
Reto Inderbitzin <mail@indr.ch>
2011-09-17 13:26:19 +0000

    BasePropertyEntity-Klasse hinzugefügt. Basis-Entity für Entities mit dynamischen Properties. Name ist zu überdenken.

---
+++ src/phiNdus.fundus.Core.Domain/Entities/BasePropertyEntity.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace phiNdus.fundus.Core.Domain.Entities
+{
+    public class BasePropertyEntity : BaseEntity
+    {
+        private ICollection<DomainPropertyValue> _properties = new List<DomainPropertyValue>();
+
+        public BasePropertyEntity() : base()
+        {
+            
+        }
+
+        public BasePropertyEntity(ICollection<DomainPropertyValue> properties) : base()
+        {
+            _properties = properties;
+        }
+
+        public bool HasProperty(DomainProperty property)
+        {
+            foreach (var each in _properties)
+            {
+                if (each.Property == property)
+                    return true;
+            }
+            return false;
+        }
+
+        public void AddProperty(DomainProperty property)
+        {
+            _properties.Add(new DomainPropertyValue(property));
+        }
+
+        public object GetPropertyValue(DomainProperty property)
+        {
+            foreach (var each in _properties)
+            {
+                if (each.Property == property)
+                    return each.Value;
+            }
+            throw new Exception("Property nicht vorhanden.");
+        }
+
+        public void SetPropertyValue(DomainProperty property, object value)
+        {
+            foreach (var each in _properties)
+                if (each.Property == property)
+                {
+                    each.Value = value;
+                    return;
+                }
+
+            throw new Exception("Property nicht vorhanden.");
+                    
+        }
+    }
+}
