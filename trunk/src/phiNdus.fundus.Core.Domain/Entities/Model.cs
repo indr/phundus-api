9da0a7c391ee563fc3a4da0babc5039b9eb0dcd4
Inder <Inder@0fb50134-772a-4728-8726-cc0728632084>
2011-09-18 10:40:18 +0000

    Parent-Child-Beziehung hinzugefügt.

--- trunk/src/phiNdus.fundus.Core.Domain/Entities/Model.cs
+++ trunk/src/phiNdus.fundus.Core.Domain/Entities/Model.cs
@@ -1,6 +1,23 @@
-﻿namespace phiNdus.fundus.Core.Domain.Entities
+﻿using Iesi.Collections.Generic;
+
+namespace phiNdus.fundus.Core.Domain.Entities
 {
     public class Model : BasePropertyEntity
     {
+        private ISet<Model> _children = new HashedSet<Model>();
+
+        public virtual ISet<Model> Children
+        {
+            get { return _children; }
+            protected set { _children = value; }
+        }
+
+        public virtual Model Parent { get; protected set; }
+
+        public virtual void AddChild(Model item)
+        {
+            _children.Add(item);
+            item.Parent = this;
+        }
     }
 }
