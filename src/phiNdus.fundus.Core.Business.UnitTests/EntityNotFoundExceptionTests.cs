9302a1547be10110b01c85720a09c73087bb700c
Reto Inderbitzin <mail@indr.ch>
2011-09-20 10:27:34 +0000

    Coverage erhöhen ;o)

---
+++ src/phiNdus.fundus.Core.Business.UnitTests/EntityNotFoundExceptionTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using phiNdus.fundus.TestHelpers;
+
+namespace phiNdus.fundus.Core.Business.UnitTests
+{
+    [TestFixture]
+    public class EntityNotFoundExceptionTests : ExceptionTestBase<EntityNotFoundException>
+    {
+        protected override EntityNotFoundException CreateSut()
+        {
+            return new EntityNotFoundException();
+        }
+
+        protected override EntityNotFoundException CreateSut(string message)
+        {
+            return new EntityNotFoundException(message);
+        }
+
+        protected override EntityNotFoundException CreateSut(string message, Exception innerException)
+        {
+            return new EntityNotFoundException(message, innerException);
+        }
+    }
+}
---
+++ tests/phiNdus.fundus.TestHelpers/ExceptionTestBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace phiNdus.fundus.TestHelpers
+{
+    public abstract class ExceptionTestBase<T> where T : Exception
+    {
+        protected abstract T CreateSut();
+        protected abstract T CreateSut(string message);
+        protected abstract T CreateSut(string message, Exception innerException);
+
+        [Test]
+        public virtual void Can_create()
+        {
+            var sut = CreateSut();
+            Assert.That(sut, Is.Not.Null);
+        }
+        
+        [Test]
+        public virtual void Can_create_with_message()
+        {
+            var sut = CreateSut("message");
+            Assert.That(sut, Is.Not.Null);
+            Assert.That(sut.Message, Is.EqualTo("message"));
+        }
+
+        [Test]
+        public virtual void Can_create_with_message_and_inner_exception()
+        {
+            var innerException = new Exception();
+            var sut = CreateSut("message", innerException);
+            Assert.That(sut, Is.Not.Null);
+            Assert.That(sut.Message, Is.EqualTo("message"));
+            Assert.That(sut.InnerException, Is.SameAs(innerException));
+        }
+
+        [Test]
+        public virtual void Is_derived_from_Exception()
+        {
+            var sut = CreateSut();
+            Assert.That(sut, Is.AssignableTo(typeof (Exception)));
+        }
+    }
+}
