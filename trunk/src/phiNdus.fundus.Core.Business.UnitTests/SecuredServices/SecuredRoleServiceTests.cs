b450d80ee7ed569ee4752f6d7825de400ab5b44b
Jac <Jac@0fb50134-772a-4728-8726-cc0728632084>
2011-09-20 20:29:30 +0000

    Service für das Abholen der verfügbaren Rollen / UserModel um Rollen-Liste erweitert / Edit-View auf UserModel umgestellt / Weiteres Sprachproblem bzgl. AccountModelTets behoben (hoffentlich jetzt alle, kanns bei mir nicht reproduzieren) / RoleAssemblerTest in RoleAssemblerTests umbenennt (Konsistenz)

---
+++ trunk/src/phiNdus.fundus.Core.Business.UnitTests/SecuredServices/SecuredRoleServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using phiNdus.fundus.Core.Business.SecuredServices;
+
+namespace phiNdus.fundus.Core.Business.UnitTests.SecuredServices {
+    [TestFixture]
+    public class SecuredRoleServiceTests : BaseTestFixture {
+
+        [SetUp]
+        public void SetUpSut() {
+            this.Sut = new SecuredRoleService();
+        }
+
+        protected SecuredRoleService Sut { get; set; }
+
+        [Test]
+        public void Get_roles_returns_existing_roles() {
+            var roles = this.Sut.GetRoles(null);
+
+            Assert.That(roles.Count(), Is.EqualTo(2));
+            Assert.That(roles.SingleOrDefault(r => r.Name == "Administrator"), Is.Not.Null);
+            Assert.That(roles.SingleOrDefault(r => r.Name == "Benutzer"), Is.Not.Null);
+        }
+    }
+}
--- trunk/src/phiNdus.fundus.Core.Web/Controllers/UserController.cs
+++ trunk/src/phiNdus.fundus.Core.Web/Controllers/UserController.cs
@@ -2,12 +2,22 @@
 using System.Web.Mvc;
 using phiNdus.fundus.Core.Business.SecuredServices;
 using Rhino.Commons;
+using phiNdus.fundus.Core.Web.Models;
+using System.Linq;
 
 namespace phiNdus.fundus.Core.Web.Controllers
 {
     [Authorize]
     public class UserController : Controller
     {
+        private IUserService UserService {
+            get { return IoC.Resolve<IUserService>(); }
+        }
+
+        private IRoleService RoleService {
+            get { return IoC.Resolve<IRoleService>(); }
+        }
+
         //
         // GET: /User/
 
@@ -21,8 +31,9 @@ namespace phiNdus.fundus.Core.Web.Controllers
 
         public ActionResult List()
         {
-            var userService = IoC.Resolve<IUserService>();
-            return View(userService.GetUsers(Session.SessionID));
+            //var userService = IoC.Resolve<IUserService>();
+            //return View(userService.GetUsers(Session.SessionID));
+            return View(this.UserService.GetUsers(Session.SessionID));
         }
 
         //
@@ -32,8 +43,9 @@ namespace phiNdus.fundus.Core.Web.Controllers
         {
             try
             {
-                var userService = IoC.Resolve<IUserService>();
-                return View(userService.GetUser(Session.SessionID, id));
+                //var userService = IoC.Resolve<IUserService>();
+                //return View(userService.GetUser(Session.SessionID, id));
+                return View(this.UserService.GetUser(Session.SessionID, id));
             }
                 // TODO: Logging
                 // TODO: Exception-Handling
@@ -51,8 +63,12 @@ namespace phiNdus.fundus.Core.Web.Controllers
         {
             try
             {
-                var userService = IoC.Resolve<IUserService>();
-                return View(userService.GetUser(Session.SessionID, id));
+                var user = this.UserService.GetUser(Session.SessionID, id);
+                var roles = this.RoleService.GetRoles(Session.SessionID);
+
+                return View(UserModel.FromDto(user, roles));
+                //var userService = IoC.Resolve<IUserService>();
+                //return View(userService.GetUser(Session.SessionID, id));
             }
                 // TODO: Logging
                 // TODO: Exception-Handling
@@ -69,12 +85,21 @@ namespace phiNdus.fundus.Core.Web.Controllers
         [HttpPost]
         public ActionResult Edit(int id, FormCollection collection)
         {
-            var userService = IoC.Resolve<IUserService>();
-            var user = userService.GetUser(Session.SessionID, id);
+            //var userService = IoC.Resolve<IUserService>();
+            //var user = userService.GetUser(Session.SessionID, id);
+            var user = this.UserService.GetUser(Session.SessionID, id);
+            var roles = this.RoleService.GetRoles(Session.SessionID);
+            //var userModel = new UserModel();
             try
             {
                 UpdateModel(user, collection.ToValueProvider());
-                userService.UpdateUser(Session.SessionID, user);
+
+                // Todo,jac(low): Review. Der Rollenname wird manuell aktualisiert
+                // da das binding nur über die id läuft.
+                user.RoleName = roles.Single(r => r.Id == user.RoleId).Name;
+
+                this.UserService.UpdateUser(Session.SessionID, user);
+
                 return RedirectToAction("Index");
             }
                 // TODO: Logging
@@ -82,7 +107,7 @@ namespace phiNdus.fundus.Core.Web.Controllers
             catch (Exception ex)
             {
                 ModelState.AddModelError("", ex.Message);
-                return View(user);
+                return View(UserModel.FromDto(user, roles));
             }
         }
     }
