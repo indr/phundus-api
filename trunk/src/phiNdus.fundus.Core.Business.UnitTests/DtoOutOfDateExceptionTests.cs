50aea8f83b3ee494fa11cd985aaff2cf41b294a4
Inder <Inder@0fb50134-772a-4728-8726-cc0728632084>
2011-09-20 10:27:34 +0000

    Coverage erhöhen ;o)

---
+++ trunk/src/phiNdus.fundus.Core.Business.UnitTests/DtoOutOfDateExceptionTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using phiNdus.fundus.TestHelpers;
+
+namespace phiNdus.fundus.Core.Business.UnitTests
+{
+    [TestFixture]
+    public class DtoOutOfDateExceptionTests : ExceptionTestBase<DtoOutOfDateException>
+    {
+        protected override DtoOutOfDateException CreateSut()
+        {
+            return new DtoOutOfDateException();
+        }
+
+        protected override DtoOutOfDateException CreateSut(string message)
+        {
+            return new DtoOutOfDateException(message);
+        }
+
+        protected override DtoOutOfDateException CreateSut(string message, Exception innerException)
+        {
+            return new DtoOutOfDateException(message, innerException);
+        }
+    }
+}
---
+++ trunk/src/phiNdus.fundus.Core.Business.UnitTests/EmailAlreadyTakenExceptionTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using phiNdus.fundus.TestHelpers;
+
+namespace phiNdus.fundus.Core.Business.UnitTests
+{
+    [TestFixture]
+    public class EmailAlreadyTakenExceptionTests : ExceptionTestBase<EmailAlreadyTakenException>
+    {
+        protected override EmailAlreadyTakenException CreateSut()
+        {
+            return new EmailAlreadyTakenException();
+        }
+
+        protected override EmailAlreadyTakenException CreateSut(string message)
+        {
+            return new EmailAlreadyTakenException(message);
+        }
+
+        protected override EmailAlreadyTakenException CreateSut(string message, Exception innerException)
+        {
+            return new EmailAlreadyTakenException(message, innerException);
+        }
+    }
+}
