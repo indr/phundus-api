890bd4bfbdb426ad84d263b93c38ba4d8479dc74
Inder <Inder@0fb50134-772a-4728-8726-cc0728632084>
2011-09-22 20:49:50 +0000

    CliTools angepasst.

--- trunk/src/phiNdus.fundus.Core.CliTools/Program.cs
+++ trunk/src/phiNdus.fundus.Core.CliTools/Program.cs
@@ -1,37 +1,75 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 
 namespace phiNdus.fundus.Core.CliTools
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static readonly Commands Commands = new Commands();
+
+        private static void Main(string[] args)
         {
-            if (args.Length == 0)
+            if (args.Length > 0)
+                ExecuteCommandString(args);
+            else
             {
-                Console.WriteLine("No command specified.");
-                Console.ReadKey();
-                return;
+                Console.WriteLine("fundus CliTools");
+                Console.WriteLine();
+                Console.WriteLine("Type help to see available commands.");
+                Console.WriteLine("Type exit to quit.");
+                Console.WriteLine();
+
+                while(true)
+                {
+                    Console.Write("> ");
+                    var inputLine = Console.ReadLine();
+                    if ((inputLine == null) || String.IsNullOrWhiteSpace(inputLine))
+                        continue;
+
+                    // Problem Leerzeichen: show user="Vorname Nachname"
+                    var command = inputLine.Split(' ');
+
+                    if (command[0].ToLowerInvariant() == "exit")
+                        break;
+
+                    ExecuteCommandString(command);
+                }
             }
             
-            var cmds = new Commands();
-            var method = cmds.GetType().GetMethod(args[0], BindingFlags.Instance | BindingFlags.IgnoreCase | BindingFlags.Public);
-            if (method == null)
-            {
-                Console.WriteLine("Command \"" + args[0] +"\" not found.");
-                Console.WriteLine("");
-                
-                var methods = cmds.GetType().GetMethods(BindingFlags.Instance | BindingFlags.IgnoreCase | BindingFlags.Public);
-                Console.WriteLine("Available commands are:");
-                foreach(var each in methods)
-                    Console.WriteLine(each.Name);
+        }
 
-                Console.WriteLine("Try [command] help");
-                Console.ReadKey();
-                return;
+        private static void ExecuteCommandString(string[] args)
+        {
+            var method = Commands.GetType().GetMethod(args[0],
+                                                              BindingFlags.Instance | BindingFlags.IgnoreCase |
+                                                              BindingFlags.Public);
+            if (method == null)
+                ShowAvailableCommands(args[0]);
+            else {
+                method.Invoke(Commands, new object[] { Console.Out, args });
+                Console.WriteLine();
             }
-            method.Invoke(cmds, new object[] {Console.Out, args});
-            Console.ReadKey();
+        }
+
+        private static void ShowAvailableCommands(string input)
+        {
+            Console.WriteLine("Command \"" + input + "\" not found.");
+            Console.WriteLine("");
+
+            var methods =
+                Commands.GetType().GetMethods(BindingFlags.Instance | BindingFlags.IgnoreCase | BindingFlags.Public).ToList();
+
+            methods.RemoveAll(m => (m.Name == "ToString") || (m.Name == "Equals") || (m.Name == "GetType") || (m.Name == "GetHashCode"));
+            
+            Console.WriteLine("Available commands are:");
+            Console.WriteLine();
+            foreach (var each in methods)
+                Console.WriteLine(each.Name.ToLowerInvariant());
+            Console.WriteLine();
+            Console.WriteLine("Try [command] help");
+            Console.WriteLine();
         }
     }
 }
