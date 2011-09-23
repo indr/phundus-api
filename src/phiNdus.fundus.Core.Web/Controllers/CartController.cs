6da0f7e6ff7e50d1138bf8814821036c133dfbf1
Jac <Jac@0fb50134-772a-4728-8726-cc0728632084>
2011-09-23 14:19:48 +0000

    Leere View, Controller und Model für Warenkorb hinzugefügt

---
+++ src/phiNdus.fundus.Core.Web/Controllers/CartController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace phiNdus.fundus.Core.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        //
+        // GET: /Cart/
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        public ActionResult List() {
+            return View();
+        }
+
+    }
+}
---
+++ src/phiNdus.fundus.Core.Web/Models/CartModels.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace phiNdus.fundus.Core.Web.Models {
+
+
+    public class CartModel {
+
+    }
+}
