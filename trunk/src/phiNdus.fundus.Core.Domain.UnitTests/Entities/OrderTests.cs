3f81028b6f0608980c9aaa9312ce24118645248f
Inder <Inder@0fb50134-772a-4728-8726-cc0728632084>
2011-10-03 18:24:01 +0000

    Order- und OrderItem-Entity hinzugefügt.

---
+++ trunk/src/phiNdus.fundus.Core.Domain.UnitTests/Entities/OrderTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using NUnit.Framework;
+using phiNdus.fundus.Core.Domain.Entities;
+
+namespace phiNdus.fundus.Core.Domain.UnitTests.Entities
+{
+    [TestFixture]
+    public class OrderTests
+    {
+        private static Order CreateSut()
+        {
+            return new Order();
+        }
+
+        [Test]
+        public void AddItem_returns_true_and_adds_item_to_Items()
+        {
+            var sut = CreateSut();
+            var item = new OrderItem();
+            Assert.That(sut.AddItem(item), Is.True);
+            Assert.That(sut.Items, Has.Some.SameAs(item));
+        }
+
+        [Test]
+        public void AddItem_sets_Order_on_Item()
+        {
+            var sut = CreateSut();
+            var item = new OrderItem();
+            sut.AddItem(item);
+            Assert.That(item.Order, Is.SameAs(sut));
+        }
+
+        [Test]
+        public void AddItem_with_item_already_added_returns_false()
+        {
+            var sut = CreateSut();
+            var item = new OrderItem();
+            sut.AddItem(item);
+            Assert.That(sut.AddItem(item), Is.False);
+        }
+
+        [Test]
+        public void Create_assignes_empty_Items_collection()
+        {
+            var sut = new Order();
+            Assert.That(sut.Items, Is.Not.Null);
+            Assert.That(sut.Items, Has.Count.EqualTo(0));
+        }
+
+        [Test]
+        public void Create_sets_CreateDate()
+        {
+            var sut = new Order();
+            Assert.That(sut.CreateDate, Is.InRange(DateTime.Now.AddMinutes(-1), DateTime.Now.AddMinutes(1)));
+        }
+
+        [Test]
+        public void Create_sets_Id_and_Version_to_0()
+        {
+            var sut = new Order();
+            Assert.That(sut.Id, Is.EqualTo(0));
+            Assert.That(sut.Version, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void RemoveItem_returns_true_and_removes_item_from_Items()
+        {
+            var sut = CreateSut();
+            var item = new OrderItem();
+            sut.AddItem(item);
+            Assert.That(sut.RemoveItem(item), Is.True);
+            Assert.That(sut.Items, Has.No.Some.SameAs(item));
+        }
+
+        [Test]
+        public void RemoveItem_sets_Order_on_Item_to_null()
+        {
+            var sut = CreateSut();
+            var item = new OrderItem();
+            item.Order = sut;
+            sut.RemoveItem(item);
+            Assert.That(item.Order, Is.Null);
+        }
+
+        [Test]
+        public void RemoveItem_without_item_added_returns_false()
+        {
+            var sut = CreateSut();
+            var item = new OrderItem();
+            Assert.That(sut.RemoveItem(item), Is.False);
+        }
+    }
+}
---
+++ trunk/src/phiNdus.fundus.Core.Domain/Entities/Order.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Iesi.Collections.Generic;
+
+namespace phiNdus.fundus.Core.Domain.Entities
+{
+    public class Order : BaseEntity
+    {
+        private DateTime _createDate;
+        private ISet<OrderItem> _items = new HashedSet<OrderItem>();
+
+        public Order()
+        {
+            _createDate = DateTime.Now;
+        }
+
+        public virtual DateTime CreateDate
+        {
+            get { return _createDate; }
+            set { _createDate = value; }
+        }
+
+        public ISet<OrderItem> Items
+        {
+            get { return _items; }
+            set { _items = value; }
+        }
+
+        public bool AddItem(OrderItem item)
+        {
+            var result = Items.Add(item);
+            item.Order = this;
+            return result;
+        }
+
+        public bool RemoveItem(OrderItem item)
+        {
+            var result = Items.Remove(item);
+            item.Order = null;
+            return result;
+        }
+    }
+}
