c6e22a5cec312a2a53f1fe1b2a0984296b8d1c88
Reto Inderbitzin <mail@indr.ch>
2011-10-03 18:57:07 +0000

    OrderStatus hinzugefügt. Order mit Methoden zum Bewilligen und Ablehnen.

---
+++ src/phiNdus.fundus.Core.Domain.UnitTests/Entities/OrderStatusTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using phiNdus.fundus.Core.Domain.Entities;
+
+namespace phiNdus.fundus.Core.Domain.UnitTests.Entities
+{
+    [TestFixture]
+    public class OrderStatusTests
+    {
+        [Test]
+        public void Pending_is_0()
+        {
+            Assert.That((int)OrderStatus.Pending, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Approved_is_1()
+        {
+            Assert.That((int)OrderStatus.Approved, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Rejected_is_2()
+        {
+            Assert.That((int)OrderStatus.Rejected, Is.EqualTo(2));
+        }
+    }
+}
