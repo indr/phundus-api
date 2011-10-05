e8ebdd60b0b7c03a952c4d862a52c22a7094b91e
Inder <Inder@0fb50134-772a-4728-8726-cc0728632084>
2011-10-05 14:56:00 +0000

    Contract-Entity hinzugefügt.

---
+++ trunk/src/phiNdus.fundus.Core.Domain/Entities/Contract.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Iesi.Collections.Generic;
+
+namespace phiNdus.fundus.Core.Domain.Entities
+{
+    public class Contract : BaseEntity
+    {
+        private ISet<ContractItem> _items;
+
+        public Contract()
+            : this(0, 0)
+        {
+        }
+
+        public Contract(int id, int version) : base(id, version)
+        {
+            _createDate = DateTime.Now;
+            _items = new HashedSet<ContractItem>();
+        }
+
+        private DateTime _createDate;
+        public DateTime CreateDate
+        {
+            get { return _createDate; }
+            set { _createDate = value; }
+        }
+
+        public User Borrower { get; set; }
+
+        public ISet<ContractItem> Items
+        {
+            get {
+                return _items;
+            }
+            set {
+                _items = value;
+            }
+        }
+
+        public bool AddItem(ContractItem item)
+        {
+            var result = Items.Add(item);
+            item.Contract = this;
+            return result;
+        }
+
+        public bool RemoveItem(ContractItem item)
+        {
+            var result = Items.Remove(item);
+            item.Contract = null;
+            return result;
+        }
+    }
+}
---
+++ trunk/src/phiNdus.fundus.Core.Domain/Entities/ContractItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace phiNdus.fundus.Core.Domain.Entities
+{
+    public class ContractItem : BaseEntity
+    {
+        public ContractItem() : this(0, 0)
+        {
+        }
+
+        public ContractItem(int id, int version) : base(id, version)
+        {
+            
+        }
+
+        public DateTime? ReturnDate { get; set; }
+
+        public Contract Contract { get; set; }
+
+        public int Amount { get; set; }
+        public Article Article { get; set; }
+        public string InventoryCode { get; set; }
+        public string Name { get; set; }
+        
+    }
+}
