68c6878e8542b1a27c080cdf54d1c5054462c459
Reto Inderbitzin <mail@indr.ch>
2011-10-05 10:09:28 +0000

    Order-Service: Methoden um Pending-, Approved- und Rejected-Bestellungen zu erhalten.

--- src/phiNdus.fundus.Core.Domain/Repositories/OrderRepository.cs
+++ src/phiNdus.fundus.Core.Domain/Repositories/OrderRepository.cs
@@ -1,9 +1,35 @@
-﻿using phiNdus.fundus.Core.Domain.Entities;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NHibernate.Linq;
+using phiNdus.fundus.Core.Domain.Entities;
 using Rhino.Commons;
 
 namespace phiNdus.fundus.Core.Domain.Repositories
 {
     public class OrderRepository : NHRepository<Order>, IOrderRepository
     {
+        private IQueryable<Order> Orders
+        {
+            get { return Session.Query<Order>(); }
+        }
+
+        public ICollection<Order> FindPending()
+        {
+            var query = from o in Orders select o;
+            return query.ToList();
+        }
+
+        public ICollection<Order> FindApproved()
+        {
+            var query = from o in Orders select o;
+            return query.ToList();
+        }
+
+        public ICollection<Order> FindRejected()
+        {
+            var query = from o in Orders select o;
+            return query.ToList();
+        }
     }
 }
