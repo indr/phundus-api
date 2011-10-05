a78980de6e493d4f1ec002900c0b224ebe5f0395
Reto Inderbitzin <mail@indr.ch>
2011-10-05 09:22:48 +0000

    OrderService.GetOrder() hinzugefügt.

--- src/phiNdus.fundus.Core.Business.UnitTests/Services/OrderServiceTests.cs
+++ src/phiNdus.fundus.Core.Business.UnitTests/Services/OrderServiceTests.cs
@@ -1,7 +1,9 @@
 ﻿using NUnit.Framework;
 using phiNdus.fundus.Core.Business.Services;
+using phiNdus.fundus.Core.Domain.Entities;
 using phiNdus.fundus.Core.Domain.Repositories;
 using Rhino.Commons;
+using Rhino.Mocks;
 
 namespace phiNdus.fundus.Core.Business.UnitTests.Services
 {
@@ -17,21 +19,26 @@ namespace phiNdus.fundus.Core.Business.UnitTests.Services
 
             FakeUnitOfWork = GenerateAndRegisterStubUnitOfWork();
 
-            Sut = new ArticleService();
+            Sut = new OrderService();
+
+            OrderDomainObject = new Order(1, 2);
         }
 
         #endregion
 
-        protected ArticleService Sut { get; set; }
+        protected OrderService Sut { get; set; }
 
         protected IUnitOfWork FakeUnitOfWork { get; set; }
         protected IOrderRepository FakeOrderRepo { get; set; }
+
+        protected Order OrderDomainObject { get; set; }
 
         private void GenerateAndRegisterMissingStubs()
         {
             if (IoC.TryResolve<IOrderRepository>() == null)
             {
                 FakeOrderRepo = GenerateAndRegisterStub<IOrderRepository>();
+                FakeOrderRepo.Expect(x => x.Get(1)).Return(OrderDomainObject);
             }
         }
 
@@ -41,5 +48,51 @@ namespace phiNdus.fundus.Core.Business.UnitTests.Services
             var sut = new OrderService();
             Assert.That(sut, Is.Not.Null);
         }
+        
+        [Test]
+        public void GetOrder_disposes_UnitOfWork()
+        {
+            FakeUnitOfWork = GenerateAndRegisterMockUnitOfWork();
+            GenerateAndRegisterMissingStubs();
+            FakeUnitOfWork.Expect(x => x.Dispose());
+
+            Sut.GetOrder(1);
+
+            FakeUnitOfWork.VerifyAllExpectations();
+        }
+
+        [Test]
+        public void GetOrder_calls_repository_Get()
+        {
+            FakeOrderRepo = GenerateAndRegisterMock<IOrderRepository>();
+            GenerateAndRegisterMissingStubs();
+
+            FakeOrderRepo.Expect(x => x.Get(1)).Return(null);
+            Sut.GetOrder(1);
+
+            FakeOrderRepo.VerifyAllExpectations();
+        }
+
+        [Test]
+        public void GetOrder_returns_dto()
+        {
+            GenerateAndRegisterMissingStubs();
+
+            var actual = Sut.GetOrder(1);
+
+            Assert.That(actual, Is.Not.Null);
+            Assert.That(actual.Id, Is.EqualTo(1));
+            Assert.That(actual.Version, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void GetOrder_with_invalid_id_returns_null()
+        {
+            GenerateAndRegisterMissingStubs();
+
+            Assert.That(Sut.GetOrder(404), Is.Null);
+        }
+
+        
     }
 }
--- src/phiNdus.fundus.Core.Business/Services/OrderService.cs
+++ src/phiNdus.fundus.Core.Business/Services/OrderService.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using phiNdus.fundus.Core.Business.Assembler;
+using phiNdus.fundus.Core.Business.Dto;
+using phiNdus.fundus.Core.Domain.Repositories;
+using Rhino.Commons;
 
 namespace phiNdus.fundus.Core.Business.Services
 {
     public class OrderService
     {
-
+        public virtual OrderDto GetOrder(int id)
+        {
+            using (UnitOfWork.Start())
+            {
+                var order = IoC.Resolve<IOrderRepository>().Get(id);
+                if (order == null)
+                    return null;
+                return OrderAssembler.CreateDto(order);
+            }
+        }
     }
 }
