e1e9fb41aded8721e3dc018b2d9e1f0bb0fa6d20
Jac <Jac@0fb50134-772a-4728-8726-cc0728632084>
2011-10-17 21:33:31 +0000

    User für mich im Staging Script angelegt

--- trunk/sql/dml-staging.sql
+++ trunk/sql/dml-staging.sql
@@ -8,9 +8,13 @@ INSERT INTO [User] (Id, Version, RoleId, FirstName, LastName)
   VALUES (1, 1, 2, 'Dilbert', 'hat die Dinge');
 INSERT INTO [User] (Id, Version, RoleId, FirstName, LastName)
   VALUES (2, 1, 1, 'Hans', 'will sie Haben');
+INSERT INTO [User] (Id, Version, RoleId, FirstName, LastName)
+  VALUES (3, 1, 1, 'Mario', 'Jacomet');
 
 INSERT INTO [Membership] (Id, Version, SessionKey, Password, Salt, Email, IsApproved, IsLockedOut, CreateDate)
   VALUES (1, 1, NULL, 'd77d9929e85306f64b45956b05c7d767' /* 1234 */, '123ab', 'admin@example.com', 1, 0, '2011-05-10');
 INSERT INTO [Membership] (Id, Version, SessionKey, Password, Salt, Email, IsApproved, IsLockedOut, CreateDate)
   VALUES (2, 1, NULL, 'f98133418bcb271b4b428a79563a8eee' /* 1234 */, '234cd', 'user@example.com', 1, 0, '2011-05-10');
+INSERT INTO [Membership] (Id, Version, SessionKey, Password, Salt, Email, IsApproved, IsLockedOut, CreateDate)
+  VALUES (3, 1, NULL, '3d08515bccad2c145afc9477b997e2ce' /* 1234 */, 'h3gst', 'mario.jacomet@gmail.com', 1, 0, '2011-10-17');
   
