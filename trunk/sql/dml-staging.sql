7b770e90efd19c3929404684f6f7357889aca994
Jac <Jac@0fb50134-772a-4728-8726-cc0728632084>
2011-10-18 04:37:43 +0000

    Datum im Staging Script angepasst

--- trunk/sql/dml-staging.sql
+++ trunk/sql/dml-staging.sql
@@ -16,5 +16,5 @@ INSERT INTO [Membership] (Id, Version, SessionKey, Password, Salt, Email, IsAppr
 INSERT INTO [Membership] (Id, Version, SessionKey, Password, Salt, Email, IsApproved, IsLockedOut, CreateDate)
   VALUES (2, 1, NULL, 'f98133418bcb271b4b428a79563a8eee' /* 1234 */, '234cd', 'user@example.com', 1, 0, '2011-05-10');
 INSERT INTO [Membership] (Id, Version, SessionKey, Password, Salt, Email, IsApproved, IsLockedOut, CreateDate)
-  VALUES (3, 1, NULL, '3d08515bccad2c145afc9477b997e2ce' /* 1234 */, 'h3gst', 'mario.jacomet@gmail.com', 1, 0, '2011-10-17');
+  VALUES (3, 1, NULL, '3d08515bccad2c145afc9477b997e2ce' /* 1234 */, 'h3gst', 'mario.jacomet@gmail.com', 1, 0, '2011-17-10');
   
