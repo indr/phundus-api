a16c399a07fe643b674590d9c9b6ce0520b8da3e
Reto Inderbitzin <mail@indr.ch>
2012-06-25 19:38:35 +0000

    Funktionalität implementiert. Es fehlen noch Texte zum Formular und nach dem Senden. #fundus-77 Assignee lukas_mueller State Open Priority Minor Type Task

---
+++ src/phiNdus.fundus.Web/Controllers/FeedbackController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.Mvc;
+using phiNdus.fundus.Business.Gateways;
+using phiNdus.fundus.Web.Models;
+using Rhino.Commons;
+
+namespace phiNdus.fundus.Web.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new FeedbackModel();
+            if (User.Identity.IsAuthenticated)
+                model.EmailAddress = User.Identity.Name;
+            return View(model);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Index(FeedbackModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var mailGateway = IoC.Resolve<IMailGateway>();
+            var body = "Feedback von " + model.EmailAddress + Environment.NewLine + Environment.NewLine + model.Comment;
+            mailGateway.Send("mail@indr.ch,lukas.mueller@piNuts.ch", "[fundus] Feedback", body);
+            return View("Done");
+        }
+    }
+}
---
+++ src/phiNdus.fundus.Web/Models/FeedbackModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace phiNdus.fundus.Web.Models
+{
+    public class FeedbackModel
+    {
+        [Required]
+        [Email(ErrorMessage = "Ungültige E-Mail-Adresse")]
+        [DisplayName("E-Mail-Adresse")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [DisplayName("Feedback")]
+        public string Comment { get; set; }
+    }
+}
