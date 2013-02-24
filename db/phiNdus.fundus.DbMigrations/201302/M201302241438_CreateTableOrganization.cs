c88c2a364c656ae5ed5c3c8838d08e10c290bc82
Reto Inderbitzin <mail@indr.ch>
2013-02-24 13:44:05 +0000

    #fundus-76 Tabelle Organization

---
+++ db/phiNdus.fundus.DbMigrations/201302/M201302241438_CreateTableOrganization.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+
+namespace phiNdus.fundus.DbMigrations._201204
+{
+    [Migration(201302241438)]
+    public class M201302241438_CreateTableOrganization : MigrationBase
+    {
+        public override void Up()
+        {
+            Create.Table("Organization").InSchema(SchemaName)
+                .WithColumn("Id").AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn("Version").AsInt32().NotNullable()
+                .WithColumn("Name").AsString(255).NotNullable();
+
+            Execute.Sql(@"dbcc checkident('Organization', reseed, 1000)");
+            
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Organization").InSchema(SchemaName);
+        }
+    }
+}
