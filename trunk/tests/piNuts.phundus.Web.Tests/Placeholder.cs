5fa6bcfdfec67ff131e1c2952c6493ca73aff051
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2013-03-29 15:34:28 +0000

    Placeholder-MSpec-Tests.

---
+++ trunk/tests/piNuts.phundus.Web.Tests/Placeholder.cs
@@ -0,0 +1,17 @@
+﻿namespace piNuts.phundus.Web.Tests
+{
+    using Machine.Specifications;
+
+    [Subject("placeholder")]
+    public class Placeholder
+    {
+        Establish context =
+            () => { };
+
+        Because of =
+            () => { };
+
+        It should_be_true =
+            () => true.ShouldBeTrue();
+    }
+}
