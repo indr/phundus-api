fed9f3b64650436558cf1dd2d77ebb9729748642
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2013-03-30 20:50:35 +0000

    FluentMigrator aktualisiert. Visual-Studio-Crash-Recovery-Funktion scheint zu funktionieren.

---
+++ trunk/db/phiNdus.fundus.DbMigrations/Runner.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using FluentMigrator;
+using FluentMigrator.Runner;
+using FluentMigrator.Runner.Announcers;
+using FluentMigrator.Runner.Initialization;
+
+namespace phiNdus.fundus.DbMigrations
+{
+    public static class Runner
+    {
+        public class MigrationOptions : IMigrationProcessorOptions
+        {
+            public bool PreviewOnly { get; set; }
+            public int Timeout { get; set; }
+        }
+
+        public static void MigrateToLatest(string connectionString, TextWriter writer)
+        {
+            // var announcer = new NullAnnouncer();
+            var announcer = new TextWriterAnnouncer(writer);
+            var assembly = Assembly.GetExecutingAssembly();
+
+            var migrationContext = new RunnerContext(announcer)
+            {
+                Namespace = "phiNdus.fundus.DbMigrations"
+            };
+
+            var options = new MigrationOptions { PreviewOnly = true, Timeout = 60 };
+            var factory = new FluentMigrator.Runner.Processors.SqlServer.SqlServer2008ProcessorFactory();
+            var processor = factory.Create(connectionString, announcer, options);
+            var runner = new MigrationRunner(assembly, migrationContext, processor);
+            runner.MigrateUp(true);
+        }
+    }
+}
