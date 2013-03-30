da6045eb1f4bf6cc43466a24a01d1a14ea4c0cc4
Reto Inderbitzin <mail@indr.ch>
2013-03-30 19:18:21 +0000

    Verzeichnisse im Application_Start() erzeugen.

--- src/phiNdus.fundus.Web/Global.asax.cs
+++ src/phiNdus.fundus.Web/Global.asax.cs
@@ -1,45 +1,50 @@
-﻿using System.Web;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Castle.MicroKernel.Registration;
 using Castle.Windsor;
 using Castle.Windsor.Installer;
 using phiNdus.fundus.Web.Helpers;
 using phiNdus.fundus.Web.Plumbing;
-using Castle.MicroKernel.Registration;
+using Rhino.Commons;
 
-namespace phiNdus.fundus.Web {
+namespace phiNdus.fundus.Web
+{
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
 
-    public class MvcApplication : HttpApplication {
-
+    public class MvcApplication : HttpApplication
+    {
         private static IWindsorContainer _container;
 
-        public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
             filters.Add(new InvalidSessionKeyAttribute());
             filters.Add(new HandleErrorAttribute());
         }
 
-        public static void RegisterRoutes(RouteCollection routes) {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = ControllerNames.Home, action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            );
+                new {controller = ControllerNames.Home, action = "Index", id = UrlParameter.Optional}
+                // Parameter defaults
+                );
 
             routes.MapRoute("ImageStore",
                             "{controller}/{action}/{id}/{name}");
         }
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822", MessageId = "Mark members as static",
-            Justification = "Kann nicht geändert werden, da vom Framework so vorgegeben.")]
-// ReSharper disable InconsistentNaming
-        protected void Application_Start() {
-// ReSharper restore InconsistentNaming
 
-            // TODO: Erstellen der Ordner ~\App_Data\Logs und ~\Content\Images\Articles
+        protected void Application_Start()
+        {
+            Directory.CreateDirectory(Server.MapPath(@"~\App_Data\Logs"));
+            Directory.CreateDirectory(Server.MapPath(@"~\Content\Images\Articles"));
 
             // TODO: Datenbank-Migration
 
@@ -53,16 +58,14 @@ namespace phiNdus.fundus.Web {
             BootstrapContainer();
         }
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822", MessageId = "Mark members as static",
-            Justification = "Kann nicht geändert werden, da vom Framework so vorgegeben.")]
-        // Gemäss Tutorial, aber diese Methode gibts ja nicht, deshalb Dispose
-// ReSharper disable InconsistentNaming
-        protected void Application_End() {
-// ReSharper restore InconsistentNaming
+        // TODO: Gibt es diese Methode in ASP.NET MVC 4?
+        protected void Application_End()
+        {
             _container.Dispose();
         }
 
-        private static void BootstrapContainer() {
+        private static void BootstrapContainer()
+        {
             // Locking?
             _container = new WindsorContainer()
                 .Install(FromAssembly.This())
@@ -71,14 +74,14 @@ namespace phiNdus.fundus.Web {
             // HttpContext registrieren für den SessionStateManager
             _container.Register(
                 Component.For<HttpContextBase>()
-                .LifeStyle
-                .PerWebRequest
-                .UsingFactoryMethod(() => new HttpContextWrapper(HttpContext.Current)));
+                    .LifeStyle
+                    .PerWebRequest
+                    .UsingFactoryMethod(() => new HttpContextWrapper(HttpContext.Current)));
 
             var controllerFactory = new WindsorControllerFactory(_container.Kernel);
             ControllerBuilder.Current.SetControllerFactory(controllerFactory);
 
-            Rhino.Commons.IoC.Initialize(_container);
+            IoC.Initialize(_container);
         }
     }
 }
