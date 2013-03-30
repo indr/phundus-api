cfd711f5cb5c34ce90553c729a951507c0fa45e5
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2013-03-30 16:34:14 +0000

    Feedback-Acceptance-Tests

---
+++ trunk/specs/piNuts.phundus.Specs/Features/Feedback.feature
@@ -0,0 +1,36 @@
+﻿Funktionalität: Feedback
+	Um den machern von phundus etwas mitzuteilen
+	Als ein Benutzer
+	Will ich ein Feedbackformular ausfüllen
+
+
+Szenario: Feedback ohne Angabe der E-Mail-Adresse
+	Angenommen ich bin auf der Feedbackseite
+	Und ich tippe ins Feld "Feedback" "Hallo" ein
+	Wenn ich auf "Senden" drücke
+	Dann muss die Meldung "Das Feld "E-Mail-Adresse" ist erforderlich." erscheinen
+	Und muss das Feld "E-Mail-Adresse" rot sein
+
+
+Szenario: Feedback ohne Angabe des Feedbacks
+	Angenommen ich bin auf der Feedbackseite
+	Und ich tippe ins Feld "E-Mail-Adresse" "user@example.com" ein
+	Wenn ich auf "Senden" drücke
+	Dann muss die Meldung "Das Feld "Feedback" ist erforderlich." erscheinen
+	Und muss das Feld "Feedback" rot sein
+
+
+Szenario: Feedback senden
+	Angenommen ich bin auf der Feedbackseite
+	Und ich tippe ins Feld "E-Mail-Adresse" "user@test.phundus.ch" ein
+	Und ich tippe ins Feld "Feedback":
+	  """
+	  Grüsse vom Feedback-Feature-Szenario "Feedback senden"!
+
+	  Server-Url: {AppSettings.ServerUrl}
+	  Version: {Assembly.Version}
+	  """
+	Wenn ich auf "Senden" drücke
+	Dann muss die Meldung "Merci!" erscheinen
+	Und muss "user@test.phundus.ch" ein E-Mail mit dem Betreff "[phundus] Feedback" erhalten haben
+	Und muss "admin@test.phundus.ch" ein E-Mail mit dem Betreff "[phundus] Feedback" erhalten haben
---
+++ trunk/specs/piNuts.phundus.Specs/Steps/FeedbackSteps.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Configuration;
+using System.Reflection;
+using NUnit.Framework;
+using piNuts.phundus.Specs.Infrastructure;
+using TechTalk.SpecFlow;
+using WatiN.Core;
+
+namespace piNuts.phundus.Specs.Steps
+{
+    [Binding]
+    public class FeedbackSteps : StepBase
+    {
+        [Given(@"ich bin auf der Feedbackseite")]
+        public void AngenommenIchBinAufDerFeedbackseite()
+        {
+            Browser.GoTo(BaseUrl + "/feedback");
+        }
+
+        [Given(@"ich tippe ins Feld ""(.*)"" ""(.*)"" ein")]
+        public void AngenommenIchTippeInsFeldEin(string p0, string p1)
+        {
+            p1 = p1.Replace("{AppSettings.ServerUrl}", ConfigurationManager.AppSettings["ServerUrl"]);
+            p1 = p1.Replace("{Assembly.Version}", Assembly.GetExecutingAssembly().GetName().Version.ToString());
+
+            Browser.TextField(Find.ByLabelText(p0)).TypeText(p1);
+        }
+
+        [Given(@"ich tippe ins Feld ""(.*)"":")]
+        public void AngenommenIchTippeInsFeld(string p0, string multilineText)
+        {
+            AngenommenIchTippeInsFeldEin(p0, multilineText);
+        }
+
+        [When(@"ich auf ""(.*)"" drücke")]
+        public void WennIchAufDrucke(string p0)
+        {
+            Browser.Button(Find.ByValue(p0)).Click();
+        }
+
+        [Then(@"muss die Meldung ""(.*)"" erscheinen")]
+        public void DannMussNebenDemFeldErscheinen(string p0)
+        {
+            Assert.That(Browser.ContainsText(p0), Is.True);
+        }
+
+        [Then(@"muss das Feld ""(.*)"" rot sein")]
+        public void DannMussDasFeldRotSein(string p0)
+        {
+            Assert.That(Browser.Label(p => p.Text == p0).Parent.ClassName, Is.EqualTo("control-group error"));
+        }
+
+        [Then(@"muss ""(.*)"" ein E-Mail mit dem Betreff ""(.*)"" erhalten haben")]
+        public void DannMussEinFeedback_E_MailErhaltenHaben(string p0, string p1)
+        {
+            var mailbox = Mailbox.For(p0);
+            var message = mailbox.Find(p1);
+
+            if (message == null)
+                Assert.Fail(String.Format("Das E-Mail mit Betreff \"{0}\" im Postfach für \"{1}\" konnte nicht gefunden werden.",
+                    p1, p0));
+        }
+
+    }
+}
