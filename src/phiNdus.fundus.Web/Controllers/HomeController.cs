4317a885b3c8db8ce6d500caf041cea957832566
Reto Inderbitzin <mail@indr.ch>
2013-04-07 00:07:17 +0000

    phundus.ch direkt auf Shop weiterleiten.

--- src/phiNdus.fundus.Web/Controllers/HomeController.cs
+++ src/phiNdus.fundus.Web/Controllers/HomeController.cs
@@ -1,17 +1,12 @@
 ﻿namespace phiNdus.fundus.Web.Controllers
 {
     using System.Web.Mvc;
-    using Castle.Transactions;
-    using phiNdus.fundus.Domain.Repositories;
 
     public class HomeController : ControllerBase
     {
-        public IUserRepository Users { get; set; }
-
-        [Transaction]
         public virtual ActionResult Index()
         {
-            return View();
+            return RedirectToAction(ShopActionNames.Index, ControllerNames.Shop);
         }
     }
 }
