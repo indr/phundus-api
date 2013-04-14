2cfc5cef890be6a1f30337d7405d4a5a7b1f43f4
Reto Inderbitzin <mail@indr.ch>
2013-04-14 13:03:33 +0000

    Böh geht ja doch?

---
+++ src/phiNdus.fundus.Web/Controllers/WebApi/BasketController.cs
@@ -0,0 +1,25 @@
+﻿namespace phiNdus.fundus.Web.Controllers.WebApi
+{
+    using System.Web;
+    using System.Web.Http;
+    using Castle.Transactions;
+    using Domain.Repositories;
+
+    public class BasketController : ApiController
+    {
+        public IUserRepository Users { get; set; }
+        public ICartRepository Carts { get; set; }
+
+        [HttpGet]
+        [HttpPost]
+        [Authorize]
+        [Transaction]
+        public virtual void Clear()
+        {
+            var user = Users.FindByEmail(User.Identity.Name);
+            var cart = Carts.FindByCustomer(user);
+            if (cart != null)
+                cart.Clear();
+        }
+    }
+}
