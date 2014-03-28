fb40e0af0f91910ba76da4e8da1d9c63e7f654ab
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2014-03-28 13:19:49 +0000

    Phundus.Persistence hinzugefügt.

---
+++ trunk/src/Phundus.Core/Services/MembershipService.cs
@@ -0,0 +1,26 @@
+﻿namespace Phundus.Core.Services
+{
+    using phiNdus.fundus.Domain.Repositories;
+
+    public interface IMembershipService
+    {
+        void JoinMemberToOrganization(int memberId, int organizationId);
+    }
+
+    public class MembershipService : IMembershipService
+    {
+        public IMemberRepository Members { get; set; }
+
+        public IOrganizationRepository Organizations { get; set; }
+
+
+        public void JoinMemberToOrganization(int memberId, int organizationId)
+        {
+            var member = Members.FindById(memberId);
+
+            var organization = Organizations.FindById(organizationId);
+
+            member.Join(organization);
+        }
+    }
+}
