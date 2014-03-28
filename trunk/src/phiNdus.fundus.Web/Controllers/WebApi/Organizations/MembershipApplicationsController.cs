05912241bcf60c7a4dacf605d2f75f0a3ac842bb
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2014-03-28 15:34:15 +0000

    Beitritt via RESTful-Resource.

---
+++ trunk/src/phiNdus.fundus.Web/Controllers/WebApi/Organizations/MembershipApplicationsController.cs
@@ -0,0 +1,32 @@
+﻿namespace phiNdus.fundus.Web.Controllers.WebApi.Organizations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Web.Http;
+    using Phundus.Core.Services;
+
+    public class MembershipApplicationsController : ApiController
+    {
+        public IOrganizationService OrganizationService { get; set; }
+
+        public void Post(int orgId, MembershipApplicationDoc dto)
+        {
+            OrganizationService.CreateMembershipApplication(orgId, dto.UserId);
+        }
+    }
+
+    public class MembershipApplicationDoc
+    {
+        private DateTime _createdOn = DateTime.UtcNow;
+
+        public int OrgId { get; set; }
+        
+        public int UserId { get; set; }
+
+        public DateTime CreatedOn
+        {
+            get { return _createdOn; }
+            set { _createdOn = value; }
+        }
+    }
+}
