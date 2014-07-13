befb523f4ca01d1d0de50f16b55116a669051ff7
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2014-07-13 10:31:08 +0000

    IHandleCommand ohne Rückgabetyp.

---
+++ trunk/src/Phundus.Core/Model/Organizations/ApplyForMembership.cs
@@ -0,0 +1,28 @@
+﻿namespace Phundus.Core.Model.Organizations
+{
+    using Cqrs;
+    using phiNdus.fundus.Domain.Entities;
+    using phiNdus.fundus.Domain.Repositories;
+
+    public class ApplyForMembership
+    {
+        public int MemberId { get; set; }
+        public int OrganizationId { get; set; }
+    }
+
+    public class ApplyForMembershipHandler : IHandleCommand<ApplyForMembership>
+    {
+        public IOrganizationRepository Organizations { get; set; }
+
+        public IMemberRepository Members { get; set; }
+
+        public void Handle(ApplyForMembership command)
+        {
+            Organization organization = Organizations.FindById(command.OrganizationId);
+
+            User user = Members.FindById(command.MemberId);
+
+            user.Join(organization);
+        }
+    }
+}
