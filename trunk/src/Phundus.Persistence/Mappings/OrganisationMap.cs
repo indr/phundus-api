c12ea62e380403dcf89d75bad87a5de56b90d509
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2014-07-20 09:47:25 +0000

    Fluent-Mapping

---
+++ trunk/src/Phundus.Persistence/Mappings/OrganisationMap.cs
@@ -0,0 +1,15 @@
+﻿namespace Phundus.Persistence.Mappings
+{
+    using Core.OrganisationCtx.DomainModel;
+    using FluentNHibernate.Mapping;
+
+    public class OrganisationMap : ClassMap<Organisation>
+    {
+        public OrganisationMap()
+        {
+            Table("Organization");
+            Id(x => x.Id);
+            Version(x => x.Version);
+        }
+    }
+}
