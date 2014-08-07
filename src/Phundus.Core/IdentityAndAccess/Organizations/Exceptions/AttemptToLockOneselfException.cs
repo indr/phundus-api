ecaccf1b8a15bd08bf773d3d1e9ebb0579222113
Reto Inderbitzin <mail@indr.ch>
2014-08-07 05:34:36 +0000

    Versuch mit ApplicationException.

---
+++ src/Phundus.Core/IdentityAndAccess/Organizations/Exceptions/AttemptToLockOneselfException.cs
@@ -0,0 +1,12 @@
+﻿namespace Phundus.Core.IdentityAndAccess.Organizations
+{
+    using System;
+
+    public class AttemptToLockOneselfException : ApplicationException
+    {
+        public AttemptToLockOneselfException() : base("Sie können sich nicht selber aus der Organisation sperren.")
+        {
+            
+        }
+    }
+}
