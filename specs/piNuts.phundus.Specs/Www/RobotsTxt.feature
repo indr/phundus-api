8a0cdfe946d0e111335bebf4c35a40fba7faba92
Reto Inderbitzin <mail@indr.ch>
2014-09-02 06:55:56 +0000

    Added robots.txt.

---
+++ specs/piNuts.phundus.Specs/Www/RobotsTxt.feature
@@ -0,0 +1,9 @@
+﻿Funktionalität: RobotsTxt
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@isSmoker
+Szenario: robots.txt
+	Wenn /robots.txt aufgerufen wird
+	Dann muss der Http-Status 200 sein
---
+++ specs/piNuts.phundus.Specs/Www/Steps/RobotsTxtSteps.cs
@@ -0,0 +1,28 @@
+﻿namespace Phundus.Specs.Www.Steps
+{
+    using System;
+    using System.Net;
+    using Machine.Specifications;
+    using NUnit.Framework;
+    using piNuts.phundus.Specs.Steps;
+    using TechTalk.SpecFlow;
+
+    [Binding]
+    public class RobotsTxtSteps : StepBase
+    {
+        private HttpWebResponse _response;
+
+        [When(@"/robots\.txt aufgerufen wird")]
+        public void WennRobots_TxtAufgerufenWird()
+        {
+            _response = (HttpWebResponse) WebRequest.Create(BaseUrl + "/robots.txt").GetResponse();
+        }
+
+        [Then(@"muss der Http-Status (.*) sein")]
+        public void DannMussDerHttp_StatusSein(int p0)
+        {
+            Assert.That(Convert.ToInt32(_response.StatusCode), Is.EqualTo(p0));
+            _response.Close();
+        }
+    }
+}
