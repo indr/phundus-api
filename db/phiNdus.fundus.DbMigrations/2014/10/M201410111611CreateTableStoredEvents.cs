8f5a18426819b5a4cb92456bfd3f54725a87f3d7
Reto Inderbitzin <mail@indr.ch>
2014-10-11 14:36:17 +0000

    #179 StoredEvents-Tabelle hinzugefügt.

---
+++ db/phiNdus.fundus.DbMigrations/2014/10/M201410111611CreateTableStoredEvents.cs
@@ -0,0 +1,24 @@
+﻿namespace phiNdus.fundus.DbMigrations
+{
+    using System;
+    using FluentMigrator;
+
+    [Migration(201410111611)]
+    public class M201410111611CreateTableStoredEvents : MigrationBase
+    {
+        public override void Up()
+        {
+            Create.Table("StoredEvents")
+                .WithColumn("EventId").AsInt64().NotNullable().PrimaryKey().Identity()
+                .WithColumn("TypeName").AsString().NotNullable()
+                .WithColumn("OccuredOnUtc").AsDateTime().Nullable()
+                .WithColumn("AggregateId").AsGuid().Nullable()
+                .WithColumn("Serialization").AsBinary(Int32.MaxValue).Nullable();
+        }
+
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
