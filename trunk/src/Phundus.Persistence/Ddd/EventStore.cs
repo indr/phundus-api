0edbd39f3ca3406b1a8522d509f55b17481eddfc
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2014-10-11 15:26:51 +0000

    #179 StoredEvents-Repository.

--- trunk/src/Phundus.Persistence/Ddd/EventStore.cs
+++ trunk/src/Phundus.Persistence/Ddd/EventStore.cs
@@ -1,37 +1,12 @@
 ﻿namespace Phundus.Persistence.Ddd
 {
-    using System;
     using Core.Ddd;
-    using NHibernate;
 
-    public class StoredEvent
-    {
-        public StoredEvent(Guid eventId, DateTime occuredOnUtc, string name, byte[] serialization)
-        {
-            EventId = eventId;
-            OccuredOnUtc = occuredOnUtc;
-            Name = name;
-            Serialization = serialization;
-        }
-
-        public long Id { get; protected set; }
-        public Guid EventId { get; protected set; }
-        public Guid AggregateId { get; protected set; }
-        public DateTime OccuredOnUtc { get; protected set; }
-        public string Name { get; protected set; }
-        public byte[] Serialization { get; protected set; }
-    }
-
-    public class EventStore : IEventStore
+    public class EventStore : IEventStore, ISubscribeTo<DomainEvent>
     {
         public IEventSerializer Serializer { get; set; }
 
-        public Func<ISession> SessionFactory { get; set; }
-
-        protected virtual ISession Session
-        {
-            get { return SessionFactory(); }
-        }
+        public IStoredEventRepository Repository { get; set; }
 
         public void Append(DomainEvent domainEvent)
         {
@@ -40,7 +15,12 @@
             var storedEvent = new StoredEvent(domainEvent.Id, domainEvent.OccuredOnUtc,
                 domainEvent.GetType().Name, serialization);
 
-            Session.Save(storedEvent);
+            Repository.Add(storedEvent);
+        }
+
+        public void Handle(DomainEvent @event)
+        {
+            Append(@event);
         }
     }
 }
---
+++ trunk/src/Phundus.Persistence/Ddd/NhStoredEventRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace Phundus.Persistence.Ddd
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Castle.Transactions;
+    using Core.Ddd;
+
+    public class NhStoredEventRepository : NhRepositoryBase<StoredEvent>, IStoredEventRepository
+    {
+        [Transaction]
+        public IEnumerable<StoredEvent> FindAll()
+        {
+            var query = from se in Entities select se;
+            return query.ToList();
+        }
+
+        public new void Add(StoredEvent storedEvent)
+        {
+            Session.Save(storedEvent);
+        }
+    }
+}
