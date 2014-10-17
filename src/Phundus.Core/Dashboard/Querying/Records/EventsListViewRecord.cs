18f2d051c67e71e9a9b16bfbec976bf0f4339a5b
Reto Inderbitzin <mail@indr.ch>
2014-10-17 04:50:50 +0000

    #179 EventsListViewRecord hinzugefügt.

---
+++ src/Phundus.Core/Dashboard/Querying/Records/EventsListViewRecord.cs
@@ -0,0 +1,11 @@
+﻿namespace Phundus.Core.Dashboard.Querying.Records
+{
+    using System;
+
+    public class EventsListViewRecord
+    {
+        public virtual long EventId { get; set; }
+        public virtual DateTime OccuredOnUtc { get; set; }
+        public virtual string Name { get; set; }
+    }
+}
