9077c5f8262302ad289b206b5f0fa472f30f1318
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2014-10-25 07:57:50 +0000

    #182 Reservation-AR ohne Tests.

---
+++ trunk/src/Phundus.Core/Inventory/Domain.Model/Reservations/ReservationCreated.cs
@@ -0,0 +1,33 @@
+﻿namespace Phundus.Core.Inventory.Domain.Model.Reservations
+{
+    using System;
+    using System.Runtime.Serialization;
+    using Common.Domain.Model;
+
+    [DataContract]
+    public class ReservationCreated : DomainEvent
+    {
+        protected ReservationCreated()
+        {
+            
+        }
+
+        [DataMember(Order = 1)]
+        public string OrganizationId { get; protected set; }
+
+        [DataMember(Order = 2)]
+        public string ArticleId { get; protected set; }
+
+        [DataMember(Order = 3)]
+        public string ReservationId { get; protected set; }
+
+        [DataMember(Order = 4)]
+        public DateTime FromUtc { get; protected set; }
+
+        [DataMember(Order = 5)]
+        public DateTime ToUtc { get; protected set; }
+
+        [DataMember(Order = 6)]
+        public int Amount { get; protected set; }
+    }
+}
