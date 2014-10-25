4cfaa8bb1da465ca36dff97300fae5ddfa7c53e5
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2014-10-25 07:57:12 +0000

    #182 Domain.Model-Basisklassen für ES.

---
+++ trunk/src/Phundus.Common/Domain.Model/EventSourcedRootEntity.cs
@@ -0,0 +1,25 @@
+﻿namespace Phundus.Common.Domain.Model
+{
+    using System.Collections.Generic;
+
+    public abstract class EventSourcedRootEntity : EntityWithCompositeId
+    {
+        private readonly List<IDomainEvent> _mutatingEvents;
+
+        protected EventSourcedRootEntity()
+        {
+            _mutatingEvents = new List<IDomainEvent>();
+        }
+
+        private void When(IDomainEvent e)
+        {
+            (this as dynamic).Apply(e);
+        }
+
+        protected void Apply(IDomainEvent e)
+        {
+            _mutatingEvents.Add(e);
+            When(e);
+        }
+    }
+}
---
+++ trunk/src/Phundus.Common/Domain.Model/Identity.cs
@@ -0,0 +1,41 @@
+﻿namespace Phundus.Common.Domain.Model
+{
+    using System;
+
+    public abstract class Identity : IEquatable<Identity>, IIdentity
+    {
+        protected Identity()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        protected Identity(string id)
+        {
+            Id = id;
+        }
+
+        public bool Equals(Identity id)
+        {
+            if (ReferenceEquals(this, id)) return true;
+            if (ReferenceEquals(null, id)) return false;
+            return Id.Equals(id.Id);
+        }
+
+        public string Id { get; protected set; }
+
+        public override bool Equals(object anotherObject)
+        {
+            return Equals(anotherObject as Identity);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().GetHashCode()*907) + Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return GetType().Name + " [Id=" + Id + "]";
+        }
+    }
+}
