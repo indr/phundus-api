cf3463f02ad5d8781129d8d89323c85b1cc67fc0
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2014-10-26 19:35:01 +0000

    #182 CreateReservation-Command.

---
+++ trunk/src/Phundus.Core/IdentityAndAccess/Domain.Model/Users/UserId.cs
@@ -0,0 +1,11 @@
+﻿namespace Phundus.Core.IdentityAndAccess.Domain.Model.Users
+{
+    using Common.Domain.Model;
+
+    public class UserId : Identity
+    {
+        public UserId(string id) : base(id)
+        {
+        }
+    }
+}
