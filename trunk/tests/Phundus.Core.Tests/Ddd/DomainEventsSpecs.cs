4f183c9865cc07734d63cc72b90735aae42c0982
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2014-11-10 06:01:26 +0000

    #182 Test für parameterlosen Konstruktor der Domain-Events.

--- trunk/tests/Phundus.Core.Tests/Ddd/DomainEventsSpecs.cs
+++ trunk/tests/Phundus.Core.Tests/Ddd/DomainEventsSpecs.cs
@@ -12,18 +12,29 @@
     public class all_subclasses_of_domain_event
     {
         private static IEnumerable<Type> domainEventTypes;
+        private static IEnumerable<ConstructorInfo> constructorInfos;
 
         public Establish c = () =>
         {
             domainEventTypes =
                 Assembly.GetAssembly(typeof (CoreInstaller)).GetTypes().Where(p => p.IsSubclassOf(typeof (DomainEvent)));
+
+            constructorInfos = domainEventTypes.Select(
+                s => s.GetConstructor(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic, null,
+                    new Type[] {}, null));
         };
 
         public It should_have_DataContract_attribute =
             () => domainEventTypes.ShouldEachConformTo(
                 t => t.GetCustomAttributes(typeof (DataContractAttribute), false).Length == 1);
-         
+
         public It should_have_parameterless_constructor =
-            () => domainEventTypes.ShouldEachConformTo(t => t.GetConstructor(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic, null, new Type[]{}, null) != null);
+            () =>
+                domainEventTypes.ShouldEachConformTo(
+                    t => t.GetConstructor(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic, null,
+                        new Type[] {}, null) != null);
+
+        public It should_have_working_parameterless_constructor =
+            () => constructorInfos.ShouldEachConformTo(c => c.Invoke(new object[] {}) != null);
     }
 }
