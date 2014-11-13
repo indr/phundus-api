9dcae2b741418e5c7e4352dd5fbd2d4d27ed5215
Reto Inderbitzin <mail@indr.ch>
2014-11-13 19:32:25 +0000

    #182 Bestand-Tab.

---
+++ src/phiNdus.fundus.Web/Scripts/modules/ph-inventory.js
@@ -0,0 +1,17 @@
+﻿
+angular.module('ph.inventory', ['ph.resources', 'ph.domain', 'ph.ui', 'ui', 'ui.bootstrap'])
+    .config(function($routeProvider) {
+    $routeProvider
+        .when('/articles/:articleId/stocks', { controller: StocksCtrl, templateUrl: '../../clientviews/inventory/stocks.html' });
+}); // ph.inventory
+
+function StocksCtrl($scope, $routeParams, $location) {
+    $scope.articleId = $routeParams.articleId;
+
+
+    $scope.openOld = function(name) {
+        alert(name);
+
+        //$location.url(};
+    };
+}; // StockCtrl
