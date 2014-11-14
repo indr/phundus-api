f4800b1832b625d5ae612cb7f3cca94c022908c2
Reto Inderbitzin <mail@indr.ch>
2014-11-14 08:00:19 +0000

    #182 Specs: Past-Events via Serialisierung.

--- specs/Phundus.Core.Specs/Contexts/PastEvents.cs
+++ specs/Phundus.Core.Specs/Contexts/PastEvents.cs
@@ -2,16 +2,19 @@ namespace Phundus.Core.Specs.Contexts
 {
     using System.Collections.Generic;
     using Common.Domain.Model;
+    using Common.Events;
     using Common.Notifications;
 
     public class PastEvents
     {
         private readonly IList<IDomainEvent> _events = new List<IDomainEvent>();
         private readonly Container _container;
+        private readonly IEventSerializer _serializer;
 
         public PastEvents(Container container)
         {
             _container = container;
+            _serializer = new EventSerializer();
         }
 
         public IList<IDomainEvent> Events
@@ -21,7 +24,10 @@ namespace Phundus.Core.Specs.Contexts
 
         public void Add(IDomainEvent e)
         {
-            _events.Add(e);
+            byte[] serialization = _serializer.Serialize(e);
+            var deserialized = (IDomainEvent) _serializer.Deserialize(e.GetType(), e.Id, e.OccuredOnUtc, serialization);
+
+            _events.Add(deserialized);
         }
 
         public void ProjectTo<T>() where T : IDomainEventHandler
