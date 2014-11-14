0081681b527151f0380fdec9fc7f468c72e9b712
Reto Inderbitzin <mail@indr.ch>
2014-11-14 17:40:43 +0000

    #182 Fix für StockContext.

--- src/Phundus.Core/Inventory/Domain.Model/Management/StockId.cs
+++ src/Phundus.Core/Inventory/Domain.Model/Management/StockId.cs
@@ -1,15 +1,10 @@
 namespace Phundus.Core.Inventory.Domain.Model.Management
 {
-    using System;
     using Common;
     using Common.Domain.Model;
 
     public class StockId : Identity<string>
     {
-        public StockId() : base(Guid.NewGuid().ToString())
-        {
-        }
-
         public StockId(string id) : base(id)
         {
             AssertionConcern.AssertArgumentNotEmpty(id, "Stock id must be provided.");
