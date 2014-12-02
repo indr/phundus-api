5074546fecf3ecd11bdd4d9bf0717d14f7729981
Reto Inderbitzin <mail@indr.ch>
2014-12-02 14:10:38 +0000

    #197 ISaga, SagaBase, ReservationSaga mit OrderItemAdded als erster Versuch.

---
+++ src/Phundus.Common/Domain.Model/SagaBase.cs
@@ -0,0 +1,45 @@
+﻿namespace Phundus.Common.Domain.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using Cqrs;
+
+    public abstract class SagaBase : ISaga
+    {
+        private readonly ICollection<IDomainEvent> _uncommittedEvents = new Collection<IDomainEvent>();
+        private readonly ICollection<ICommand> _undispatchedCommands = new Collection<ICommand>();
+
+        public Guid Id { get; private set; }
+
+        public long Version { get; private set; }
+
+        public void Transition(IDomainEvent e)
+        {
+            When(e);
+            UncommittedEvents.Add(e);
+        }
+
+        public ICollection<IDomainEvent> UncommittedEvents
+        {
+            get { return _uncommittedEvents; }
+        }
+
+        public void ClearUncommittedEvents()
+        {
+            _uncommittedEvents.Clear();
+        }
+
+        public ICollection<ICommand> UndispatchedCommands
+        {
+            get { return _undispatchedCommands; }
+        }
+
+        public void ClearUndispatchedCommands()
+        {
+            _undispatchedCommands.Clear();
+        }
+
+        protected abstract void When(IDomainEvent e);
+    }
+}
---
+++ src/Phundus.Core/Shop/Domain.Model/Ordering/ReservationSaga.cs
@@ -0,0 +1,21 @@
+﻿namespace Phundus.Core.Shop.Domain.Model.Ordering
+{
+    using System;
+    using Common.Domain.Model;
+    using Inventory.Application.Commands;
+    using Orders.Model;
+
+    public class ReservationSaga : SagaBase
+    {
+        protected override void When(IDomainEvent e)
+        {
+            When((dynamic) e);
+        }
+
+        private void When(OrderItemAdded e)
+        {
+            UndispatchedCommands.Add(new ReserveArticle(0, 0, 0, new OrderId(e.OrderId),
+                new CorrelationId(e.OrderItemId), DateTime.UtcNow, DateTime.UtcNow.AddDays(1), 1));
+        }
+    }
+}
