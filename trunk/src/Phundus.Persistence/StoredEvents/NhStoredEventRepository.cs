fcc6fa5dfb3d7035017fdcf591521dbe0bd079a3
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2015-01-04 06:19:30 +0000

    #201 Unit-Test für ICommandDispatcher.Dispatch().

--- trunk/src/Phundus.Persistence/StoredEvents/NhStoredEventRepository.cs
+++ trunk/src/Phundus.Persistence/StoredEvents/NhStoredEventRepository.cs
@@ -5,24 +5,6 @@
     using Common.Events;
     using Core.Ddd;
 
-    public class NhSagaStoredEventRepository : NhRepositoryBase<SagaStoredEvent>, ISagaStoredEventRepository
-    {
-        public void Append(SagaStoredEvent sagaStoredEvent)
-        {
-            Session.Save(sagaStoredEvent);
-        }
-
-        public IEnumerable<SagaStoredEvent> AllStoredEventsBetween(long lowStoredEventId, long highStoredEventId)
-        {
-            var query = from se in Entities
-                        where se.EventId >= lowStoredEventId
-                              && se.EventId <= highStoredEventId
-                        select se;
-
-            return query.ToList();
-        }
-    }
-
     public class NhStoredEventRepository : NhRepositoryBase<StoredEvent>, IStoredEventRepository
     {
         public void Append(StoredEvent storedEvent)
