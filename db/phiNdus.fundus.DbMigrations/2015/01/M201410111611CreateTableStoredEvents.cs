c5ded3155d2d35e231eb425ee52198974cc19732
Reto Inderbitzin <mail@indr.ch>
2015-01-16 09:58:48 +0000

    AddColumnStockIdToArticle-Migration in eigene Datei verschoben.

--- db/phiNdus.fundus.DbMigrations/2015/01/M201410111611CreateTableStoredEvents.cs
+++ db/phiNdus.fundus.DbMigrations/2015/01/M201410111611CreateTableStoredEvents.cs
@@ -24,18 +24,4 @@
         {
         }
     }
-
-    [Migration(201501160956)]
-    public class M201501160956AddColumnStockIdToArticle : MigrationBase
-    {
-        public override void Up()
-        {
-            Alter.Table("Article").AddColumn("StockId").AsString().Nullable();
-        }
-
-        public override void Down()
-        {
-            throw new NotImplementedException();
-        }
-    }
 }
