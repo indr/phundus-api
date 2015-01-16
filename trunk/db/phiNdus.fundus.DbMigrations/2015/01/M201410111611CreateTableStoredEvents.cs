a788654b18451a00c00df43ada3f032283f248e1
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2015-01-16 09:23:36 +0000

    #211 OrderItemPeriodChanged und OrderItemQuantityChanged nicht auslösen, wenn keine Änderung vorgefallen ist.

--- trunk/db/phiNdus.fundus.DbMigrations/2015/01/M201410111611CreateTableStoredEvents.cs
+++ trunk/db/phiNdus.fundus.DbMigrations/2015/01/M201410111611CreateTableStoredEvents.cs
@@ -24,4 +24,18 @@
         {
         }
     }
+
+    [Migration(201501160956)]
+    public class M201501160956AddColumnStockIdToArticle : MigrationBase
+    {
+        public override void Up()
+        {
+            Alter.Table("Article").AddColumn("StockId").AsString().Nullable();
+        }
+
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
