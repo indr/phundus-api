c0bdbd523730693f347367e375f75ad52d8c8d90
Reto Inderbitzin <mail@indr.ch>
2015-01-16 04:34:51 +0000

    #205 AllocationData hinzugefügt.

---
+++ src/Phundus.Core/Inventory/Application/Data/AllocationData.cs
@@ -0,0 +1,55 @@
+﻿namespace Phundus.Core.Inventory.Application.Data
+{
+    using System;
+
+    public class AllocationData
+    {
+        private Guid _allocationId;
+        private int _articleId;
+        private int _organizationId;
+        private string _stockId;
+
+        public AllocationData(Guid allocationId, int organizationId, int articleId, string stockId)
+        {
+            _allocationId = allocationId;
+            _organizationId = organizationId;
+            _articleId = articleId;
+            _stockId = stockId;
+        }
+
+        protected AllocationData()
+        {
+        }
+
+        public virtual Guid AllocationId
+        {
+            get { return _allocationId; }
+            set { _allocationId = value; }
+        }
+
+        public virtual int ConcurrencyVersion { get; set; }
+
+        public virtual int OrganizationId
+        {
+            get { return _organizationId; }
+            set { _organizationId = value; }
+        }
+
+        public virtual int ArticleId
+        {
+            get { return _articleId; }
+            set { _articleId = value; }
+        }
+
+        public virtual string StockId
+        {
+            get { return _stockId; }
+            set { _stockId = value; }
+        }
+
+        public virtual DateTime FromUtc { get; set; }
+        public virtual DateTime ToUtc { get; set; }
+        public virtual int Quantity { get; set; }
+        public virtual string Status { get; set; }
+    }
+}
---
+++ src/Phundus.Persistence/Inventory/Mappings/AllocationDataMap.cs
@@ -0,0 +1,22 @@
+﻿namespace Phundus.Persistence.Inventory.Mappings
+{
+    using Core.Inventory.Application.Data;
+    using FluentNHibernate.Mapping;
+
+    public class AllocationDataMap : ClassMap<AllocationData>
+    {
+        public AllocationDataMap()
+        {
+            Id(x => x.AllocationId).GeneratedBy.Assigned();
+            Version(x => x.ConcurrencyVersion);
+
+            Map(x => x.ArticleId);
+            Map(x => x.FromUtc);
+            Map(x => x.OrganizationId);
+            Map(x => x.Quantity);
+            Map(x => x.Status);
+            Map(x => x.StockId);
+            Map(x => x.ToUtc);
+        }
+    }
+}
