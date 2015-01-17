060398814ef48ce81cb5af93c9d0ef59d3c68ddb
Reto Inderbitzin <mail@indr.ch>
2015-01-17 09:24:16 +0000

    #214 Period.ShiftDays() und Tests für Equality hinzugefügt.

---
+++ tests/Phundus.Common.Tests/Domain.Model/PeriodSpecs.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Phundus.Common.Tests.Domain.Model
+{
+    using Common.Domain.Model;
+    using Machine.Specifications;
+
+    [Subject(typeof(Period))]
+    public class when_comparing_two_different_periods
+    {
+        private Establish ctx = () =>
+        {
+            _period1 = Period.FromTodayToTomorrow;
+            _period2 = Period.FromTodayToTomorrow.ShiftDays(1);
+        };
+
+        private static Period _period1;
+        private static Period _period2;
+
+        private Because of = () =>
+        {
+            _result = Equals(_period1, _period2);
+        };
+
+        public It should_be_false = () => _result.ShouldBeFalse();
+        private static bool _result;
+    }
+
+    [Subject(typeof(Period))]
+    public class when_comparing_two_same_periods
+    {
+        private Establish ctx = () =>
+        {
+            _period1 = Period.FromTodayToTomorrow;
+            _period2 = Period.FromTodayToTomorrow;
+        };
+
+        private static Period _period1;
+        private static Period _period2;
+
+        private Because of = () =>
+        {
+            _result = Equals(_period1, _period2);
+        };
+
+        public It should_be_true = () => _result.ShouldBeTrue();
+        private static bool _result;
+    }
+}
