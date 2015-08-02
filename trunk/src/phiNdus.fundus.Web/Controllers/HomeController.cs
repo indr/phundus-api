f343f6fc500338a1e6a5a2bdf4bde1326fb30ffb
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2015-08-02 13:40:18 +0000

    Base-Tag mit URL einfügen. Es huere gwörg.

--- trunk/src/phiNdus.fundus.Web/Controllers/HomeController.cs
+++ trunk/src/phiNdus.fundus.Web/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Text;
+    using System.Web;
     using System.Web.Mvc;
 
     public class HomeController : ControllerBase
@@ -10,6 +11,12 @@
         public virtual ActionResult Index()
         {
             var content = System.IO.File.ReadAllText(Server.MapPath("~/index.html"));
+
+            var idx = content.IndexOf(@"<link", StringComparison.InvariantCulture);
+            if (idx < 0)
+                throw new Exception("Fehler beim Einfügen des base-Tags: Die Einfügeposition, resp. ein link-Tag konnte nicht gefunden werden. Siehe Methode HomeController.Index().");
+
+            content = content.Insert(idx, RenderPartialViewToString("_BaseUrl") + " ");
             return Content(content, "text/html", Encoding.UTF8);
         }
 
