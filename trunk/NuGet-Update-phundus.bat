4305c6e3728c5ee7a7ef3ed1048db2910a203b42
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2015-08-16 21:07:03 +0000

    Updated phundus.spa and phundus.api.

---
+++ trunk/NuGet-Update-phundus.bat
@@ -0,0 +1,3 @@
+.nuget\NuGet.exe update src\phiNdus.fundus.Web\packages.config -Source http://nuget.phundus.ch/nuget -Id phundus.api -Verbosity detailed
+.nuget\NuGet.exe update src\phiNdus.fundus.Web\packages.config -Source http://nuget.phundus.ch/nuget -Id phundus.spa -Verbosity detailed
+pause
