9c777fc6d05d759cd5b13b43e84fe183137f6396
Reto Inderbitzin <mail@indr.ch>
2015-08-17 08:08:18 +0000

    Updated phundus.api and added smoke test for /node/status.

--- specs/piNuts.phundus.Specs/Www/Steps/RobotsTxtSteps.cs
+++ specs/piNuts.phundus.Specs/Www/Steps/RobotsTxtSteps.cs
@@ -1,8 +1,9 @@
 ﻿namespace Phundus.Specs.Www.Steps
 {
     using System;
+    using System.IO;
     using System.Net;
-    using Machine.Specifications;
+    using System.Text.RegularExpressions;
     using NUnit.Framework;
     using piNuts.phundus.Specs.Steps;
     using TechTalk.SpecFlow;
@@ -10,6 +11,7 @@
     [Binding]
     public class RobotsTxtSteps : StepBase
     {
+        private string _body;
         private HttpWebResponse _response;
 
         [When(@"/robots\.txt aufgerufen wird")]
@@ -18,11 +20,28 @@
             _response = (HttpWebResponse) WebRequest.Create(BaseUrl + "/robots.txt").GetResponse();
         }
 
+        [When(@"/node/status aufgerufen wird")]
+        public void WennNodeStatusAufgerufenWird()
+        {
+            //_response = (HttpWebResponse)WebRequest.Create(BaseUrl + "/Content/status.txt").GetResponse();
+            _response = (HttpWebResponse) WebRequest.Create(NodeUrl + "/status").GetResponse();
+
+            var stream = _response.GetResponseStream();
+            if (null != stream) _body = new StreamReader(stream).ReadToEnd();
+        }
+
         [Then(@"muss der Http-Status (.*) sein")]
         public void DannMussDerHttp_StatusSein(int p0)
         {
             Assert.That(Convert.ToInt32(_response.StatusCode), Is.EqualTo(p0));
             _response.Close();
+        }
+
+        [Then(@"JSON status: 'OK'")]
+        public void DannJSONStatusOK()
+        {
+            var regex = new Regex(@"status:\s*'OK'\s*,");
+            Assert.That(regex.Match(_body).Success, Is.True, _body);
         }
     }
 }
--- src/phiNdus.fundus.Web/node/config/routes.js
+++ src/phiNdus.fundus.Web/node/config/routes.js
@@ -7,10 +7,14 @@
 var mount = require('koa-mount');
 
 module.exports = function(app) {
+  
+  // I don't know how to prefix routes with koa-router and koa-mount
+  var prefix = '/node';
 
 	// YEOMAN INJECT ROUTES BELOW
-	app.use(mount('/status', require('../resources/status')));
-  app.use(mount('/', require('../resources/root')));
+	app.use(mount(prefix + '/status', require('../resources/status')));
+  app.use(mount(prefix + '/', require('../resources/root')));
+
 
 
 };
