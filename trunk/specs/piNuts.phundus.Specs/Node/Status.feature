dcb1b49a00275f5cbbf566c98be978dedf7626f1
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2015-12-19 10:35:52 +0000

    Node-Tests entfernt.

--- trunk/specs/piNuts.phundus.Specs/Node/Status.feature
+++
@@ -1,7 +0,0 @@
-﻿Funktionalität: Status
-
-@isSmoker
-Szenario: /api/v1/status
-	Wenn /api/v1/status aufgerufen wird
-	Dann muss der Http-Status 200 sein
-	Und JSON status: 'OK'
--- trunk/specs/piNuts.phundus.Specs/Www/Steps/RobotsTxtSteps.cs
+++ trunk/specs/piNuts.phundus.Specs/Www/Steps/RobotsTxtSteps.cs
@@ -10,8 +10,7 @@
 
     [Binding]
     public class RobotsTxtSteps : StepBase
-    {
-        private string _body;
+    {        
         private HttpWebResponse _response;
 
         [When(@"/robots\.txt aufgerufen wird")]
@@ -20,28 +19,11 @@
             _response = (HttpWebResponse) WebRequest.Create(BaseUrl + "/robots.txt").GetResponse();
         }
 
-        [When(@"/api/v1/status aufgerufen wird")]
-        public void WennApiV1StatusAufgerufenWird()
-        {
-            //_response = (HttpWebResponse)WebRequest.Create(BaseUrl + "/Content/status.txt").GetResponse();
-            _response = (HttpWebResponse) WebRequest.Create(NodeUrl + "/status").GetResponse();
-
-            var stream = _response.GetResponseStream();
-            if (null != stream) _body = new StreamReader(stream).ReadToEnd();
-        }
-
         [Then(@"muss der Http-Status (.*) sein")]
         public void DannMussDerHttp_StatusSein(int p0)
         {
             Assert.That(Convert.ToInt32(_response.StatusCode), Is.EqualTo(p0));
             _response.Close();
-        }
-
-        [Then(@"JSON status: 'OK'")]
-        public void DannJSONStatusOK()
-        {
-            var regex = new Regex(@"""status"":\s*""OK""\s*,");
-            Assert.That(regex.Match(_body).Success, Is.True, _body);
         }
     }
 }
