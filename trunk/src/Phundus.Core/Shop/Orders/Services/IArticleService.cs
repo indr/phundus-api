83f1aff1ebd022ea31a435bb716d02115b6ee040
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2015-12-26 22:47:08 +0000

    ArticleWrapper und IArticleService anstelle Article-Aggregate und IArticleRepository.

---
+++ trunk/src/Phundus.Core/Shop/Orders/Services/IArticleService.cs
@@ -0,0 +1,29 @@
+namespace Phundus.Core.Shop.Orders.Services
+{
+    using Common;
+    using Inventory.Articles.Repositories;
+    using Model;
+
+    public interface IArticleService
+    {
+        ArticleWrapper GetById(int articleId);
+    }
+
+    public class ArticleService : IArticleService
+    {
+        private readonly IArticleRepository _articleRepository;
+
+        public ArticleService(IArticleRepository articleRepository)
+        {
+            AssertionConcern.AssertArgumentNotNull(articleRepository, "ArticleRepository must be provided.");
+
+            _articleRepository = articleRepository;
+        }
+
+        public ArticleWrapper GetById(int articleId)
+        {
+            var article = _articleRepository.GetById(articleId);
+            return new ArticleWrapper(article);
+        }
+    }
+}
