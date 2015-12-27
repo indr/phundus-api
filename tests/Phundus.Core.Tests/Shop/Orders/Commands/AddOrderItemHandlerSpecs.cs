c30d82c827decdc6be4a53df83e50e3b93b1e964
Reto Inderbitzin <mail@indr.ch>
2015-12-27 10:45:15 +0000

    Fixes unit tests.

--- tests/Phundus.Core.Tests/Shop/Orders/Commands/AddOrderItemHandlerSpecs.cs
+++ tests/Phundus.Core.Tests/Shop/Orders/Commands/AddOrderItemHandlerSpecs.cs
@@ -1,15 +1,12 @@
 ﻿namespace Phundus.Core.Tests.Shop.Orders.Commands
 {
     using System;
-    using Core.Inventory.Articles.Model;
     using Core.Shop.Orders.Commands;
     using Core.Shop.Orders.Model;
     using developwithpassion.specifications.extensions;
     using Machine.Fakes;
     using Machine.Specifications;
     using Rhino.Mocks;
-    using Rhino.Mocks.Constraints;
-    using Article = Core.Inventory.Articles.Model.Article;
 
     [Subject(typeof (AddOrderItemHandler))]
     public class when_add_order_item_is_handled : order_handler_concern<AddOrderItem, AddOrderItemHandler>
@@ -17,13 +14,17 @@
         private const int initiatorId = 2;
         private const int orderId = 3;
         private const int articleId = 4;
+        private static Lessor lessor;
         private static Order order;
 
         public Establish c = () =>
         {
-            order = new Order(organization, BorrowerFactory.Create());
+            var owner = new Owner(Guid.NewGuid(), "Owner");
+            lessor = new Lessor(owner.OwnerId, "Lessor");
+            order = new Order(lessor, BorrowerFactory.Create());
             orders.setup(x => x.GetById(orderId)).Return(order);
-            articles.setup(x => x.GetById(organization.Id, articleId)).Return(new Article(organization.Id, "Artikel"));
+            var article = new Article(articleId, organization.Id, owner, "Artikel", 1.0m);
+            articles.setup(x => x.GetById(articleId)).Return(article);
             command = new AddOrderItem
             {
                 Amount = 10,
@@ -36,12 +37,12 @@
         };
 
         public It should_ask_for_chief_privileges =
-            () => memberInRole.WasToldTo(x => x.ActiveChief(organization.Id, initiatorId));
-
-        public It should_set_order_item_id =
-            () => command.OrderItemId.ShouldNotEqual(Guid.Empty);
+            () => memberInRole.WasToldTo(x => x.ActiveChief(lessor.LessorId, initiatorId));
 
         public It should_publish_order_item_added =
             () => publisher.WasToldTo(x => x.Publish(Arg<OrderItemAdded>.Is.NotNull));
+
+        public It should_set_order_item_id =
+            () => command.OrderItemId.ShouldNotEqual(Guid.Empty);
     }
 }
