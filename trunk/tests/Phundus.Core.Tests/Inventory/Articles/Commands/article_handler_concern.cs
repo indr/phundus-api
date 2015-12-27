40c684697b38071031c3d8b859bb589e9dfdbde4
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2015-12-27 18:59:25 +0000

    Fixes unit tests.

--- trunk/tests/Phundus.Core.Tests/Inventory/Articles/Commands/article_handler_concern.cs
+++ trunk/tests/Phundus.Core.Tests/Inventory/Articles/Commands/article_handler_concern.cs
@@ -3,6 +3,7 @@ namespace Phundus.Core.Tests.Inventory
     using Core.Cqrs;
     using Core.IdentityAndAccess.Queries;
     using Core.Inventory.Articles.Repositories;
+    using Core.Inventory.Services;
     using Machine.Specifications;
 
     public abstract class article_handler_concern<TCommand, THandler> : handler_concern<TCommand, THandler> where THandler : class, IHandleCommand<TCommand>
@@ -11,10 +12,13 @@ namespace Phundus.Core.Tests.Inventory
 
         protected static IArticleRepository repository;
 
+        protected static IOwnerService ownerService;
+
         protected Establish dependencies = () =>
         {
             memberInRole = depends.on<IMemberInRole>();
             repository = depends.on<IArticleRepository>();
+            ownerService = depends.on<IOwnerService>();
         };
     }
 }
