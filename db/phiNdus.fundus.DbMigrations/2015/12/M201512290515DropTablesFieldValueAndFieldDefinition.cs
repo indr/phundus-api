3cac7dd90ca6cca4fa6fd667bd38920ee851bbb9
Reto Inderbitzin <mail@indr.ch>
2015-12-29 04:19:36 +0000

    Tabellen FieldValue und FieldDefinition entfernt.

---
+++ db/phiNdus.fundus.DbMigrations/2015/12/M201512290515DropTablesFieldValueAndFieldDefinition.cs
@@ -0,0 +1,20 @@
+namespace phiNdus.fundus.DbMigrations
+{
+    using System;
+    using FluentMigrator;
+
+    [Migration(201512290515)]
+    public class M201512290515DropTablesFieldValueAndFieldDefinition : MigrationBase
+    {
+        public override void Up()
+        {
+            Delete.Table("FieldValue").InSchema(SchemaName);
+            Delete.Table("FieldDefinition").InSchema(SchemaName);
+        }
+
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
