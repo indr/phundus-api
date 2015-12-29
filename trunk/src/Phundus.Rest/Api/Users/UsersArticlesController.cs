77e74a58f49f316e879a829abf28573de50ed1e2
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2015-12-29 07:11:36 +0000

    UsersArticlesController.Post hinzugefügt.

--- trunk/src/Phundus.Rest/Api/Users/UsersArticlesController.cs
+++ trunk/src/Phundus.Rest/Api/Users/UsersArticlesController.cs
@@ -1,5 +1,6 @@
 namespace Phundus.Rest.Api.Users
 {
+    using System;
     using System.Net;
     using System.Net.Http;
     using AttributeRouting;
@@ -9,22 +10,20 @@ namespace Phundus.Rest.Api.Users
     using Core.IdentityAndAccess.Queries;
     using Core.Inventory.Articles.Commands;
     using Core.Inventory.Queries;
+    using Newtonsoft.Json;
 
     [RoutePrefix("api/users/{userId}/articles")]
     public class UsersArticlesController : ApiControllerBase
     {
         private readonly IArticleQueries _articleQueries;
-        private readonly IMemberInRole _memberInRole;
         private readonly IUserQueries _userQueries;
 
-        public UsersArticlesController(IMemberInRole memberInRole, IArticleQueries articleQueries,
+        public UsersArticlesController(IArticleQueries articleQueries,
             IUserQueries userQueries)
         {
-            AssertionConcern.AssertArgumentNotNull(memberInRole, "MemberInRole must be provided.");
             AssertionConcern.AssertArgumentNotNull(articleQueries, "ArticleQueries must be provided.");
             AssertionConcern.AssertArgumentNotNull(userQueries, "UserQueries must be provided.");
 
-            _memberInRole = memberInRole;
             _articleQueries = articleQueries;
             _userQueries = userQueries;
         }
@@ -33,24 +32,47 @@ namespace Phundus.Rest.Api.Users
         [Transaction]
         public virtual HttpResponseMessage Get(int userId)
         {
-            var user = _userQueries.GetById(userId);
-            if (user.Id != CurrentUserId)
-                throw new AuthorizationException();
+            var currentUserGuid = EnforceCurrentUser(userId);
 
-            var result = _articleQueries.FindByOwnerId(user.Guid);
+            var result = _articleQueries.FindByOwnerId(currentUserGuid);
             return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+        
+        [POST("")]
+        [Transaction]
+        public virtual UsersArticlesOkResponseContent Post(int userId, UsersArticlesPostRequestContent requestContent)
+        {
+            throw new NotImplementedException();
         }
 
         [DELETE("{articleId}")]
         [Transaction]
         public virtual HttpResponseMessage Delete(int userId, int articleId)
         {
-            var user = _userQueries.GetById(userId);
-            if (user.Id != CurrentUserId)
-                throw new AuthorizationException();
+            EnforceCurrentUser(userId);
 
             Dispatcher.Dispatch(new DeleteArticle {ArticleId = articleId, InitiatorId = CurrentUserId});
             return Request.CreateResponse(HttpStatusCode.OK);
         }
+
+        private Guid EnforceCurrentUser(int userId)
+        {
+            var user = _userQueries.GetById(userId);
+            if (user.Id != CurrentUserId)
+                throw new AuthorizationException();
+            return user.Guid;
+        }
+    }
+
+    public class UsersArticlesPostRequestContent
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
+    public class UsersArticlesOkResponseContent
+    {
+        [JsonProperty("articleId")]
+        public int ArticleId { get; set; }
     }
 }
