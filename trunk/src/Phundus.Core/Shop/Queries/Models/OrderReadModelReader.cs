00b1bbe21190e421e75689726d77fb78c9b17965
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2015-12-31 18:08:21 +0000

    IOrderQueries.Query() mit Auth-Filter.

--- trunk/src/Phundus.Core/Shop/Queries/Models/OrderReadModelReader.cs
+++ trunk/src/Phundus.Core/Shop/Queries/Models/OrderReadModelReader.cs
@@ -4,6 +4,7 @@
     using System.Collections.Generic;
     using System.Data.Linq;
     using System.Linq;
+    using IdentityAndAccess.Queries;
     using Inventory.Services;
 
     public class OrderReadModelReader : ReadModelReaderBase, IOrderQueries
@@ -18,6 +19,12 @@
                 return op;
             }
         }
+
+        public IAvailabilityService AvailabilityService { get; set; }
+
+        public IUserQueries UserQueries { get; set; }
+
+        public IMembershipQueries MembershipQueries { get; set; }
 
         public OrderDto SingleByOrderId(int orderId, int currentUserId)
         {
@@ -40,7 +47,7 @@
 
             var query = from o in ctx.OrderDtos
                 join m in ctx.MembershipDtos on
-                    new { Role = MembershipRoleDto.Chief, UserId = currentUserId}
+                    new {Role = MembershipRoleDto.Chief, UserId = currentUserId}
                     equals new {m.Role, m.UserId}
                 where (o.Id == orderId && o.Lessor_LessorId == organizationId && m.OrganizationGuid == organizationId)
                 select o;
@@ -50,20 +57,6 @@
             CalculateAvailabilities(result);
 
             return result;
-        }
-
-        public IAvailabilityService AvailabilityService { get; set; }
-
-        private void CalculateAvailabilities(OrderDto orderDto)
-        {
-            if (orderDto == null)
-                return;
-
-            foreach (var each in orderDto.Items)
-            {
-                each.IsAvailable = AvailabilityService.IsArticleAvailable(each.ArticleId, each.FromUtc, each.ToUtc,
-                    each.Amount, each.Id);
-            }
         }
 
         public IEnumerable<OrderDto> ManyByUserId(int userId)
@@ -80,11 +73,66 @@
         {
             var ctx = CreateCtx();
             return from o in ctx.OrderDtos
-                        join m in ctx.MembershipDtos on
-                            new { Role = MembershipRoleDto.Chief, UserId = currentUserId }
-                            equals new { m.Role, m.UserId }
-                        where (o.Lessor_LessorId == organizationId && m.OrganizationGuid == organizationId)
-                        select o;
+                join m in ctx.MembershipDtos on
+                    new {Role = MembershipRoleDto.Chief, UserId = currentUserId}
+                    equals new {m.Role, m.UserId}
+                where (o.Lessor_LessorId == organizationId && m.OrganizationGuid == organizationId)
+                select o;
+        }
+
+        public IEnumerable<OrderDto> Query(int currentUserId, int? orderId, int? queryUserId, Guid? queryOrganizationId)
+        {
+            var currentUserGuid = UserQueries.GetById(currentUserId).Guid;
+            var queryUserGuid = (Guid?) null;
+            if (queryUserId.HasValue)
+            {
+                queryUserGuid = UserQueries.GetById(queryUserId.Value).Guid;
+            }
+
+            var currentUsersManagerGuids =
+                MembershipQueries.ByUserId(currentUserId)
+                    .Where(p => p.MembershipRole == "Chief")
+                    .Select(s => s.OrganizationGuid);
+
+            var ctx = CreateCtx();
+            var q = from o in ctx.OrderDtos
+                where (
+                    // Auth restrictions
+                    (
+                        // current user is borrower or lessor
+                        ((o.Borrower_Id == currentUserId || o.Lessor_LessorId == currentUserGuid))
+                        ||
+                        // Or: current user is manager in lessor organization
+                        (currentUsersManagerGuids.Contains(o.Lessor_LessorId))
+                        )
+                    &&
+                    // Query restrictions
+                    (
+                        // orderId is not queried, or order id is query order id
+                        (orderId == null || (o.Id == orderId))
+                        &&
+                        // user is not queried, or borrower or lessor is query user
+                        (queryUserId == null
+                         || (o.Borrower_Id == queryUserId || o.Lessor_LessorId == queryUserGuid))
+                        &&
+                        // organization is not queried, or lessor is query organization
+                        (queryOrganizationId == null || (o.Lessor_LessorId == queryOrganizationId))
+                        ))
+                select o;
+
+            return q;
+        }
+
+        private void CalculateAvailabilities(OrderDto orderDto)
+        {
+            if (orderDto == null)
+                return;
+
+            foreach (var each in orderDto.Items)
+            {
+                each.IsAvailable = AvailabilityService.IsArticleAvailable(each.ArticleId, each.FromUtc, each.ToUtc,
+                    each.Amount, each.Id);
+            }
         }
     }
 }
