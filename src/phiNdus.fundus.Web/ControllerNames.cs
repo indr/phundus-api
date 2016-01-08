40e50dcd102d583ec13004354f3cb5c86957f605
Reto Inderbitzin <mail@indr.ch>
2016-01-08 14:03:10 +0000

    Actions ChangeEmail und ChangePassword entfernt.

--- src/phiNdus.fundus.Web/ControllerNames.cs
+++ src/phiNdus.fundus.Web/ControllerNames.cs
@@ -2,22 +2,16 @@
 {
     public class ControllerNames
     {
-        public static string Account { get { return @"account";  } }
-        public static string Articles { get { return @"article"; } }
+        
         public static string Cart { get { return @"cart"; } }        
         public static string Home { get { return @"home"; } }
-        public static string Orders { get { return @"order"; } }
         public static string Shop { get { return @"shop"; } }
-        public static string Users { get { return @"users"; } }
         public static string Organizations { get { return @"organizations"; } }
     }
     
     public class AccountActionNames
     {
         public static string Validation { get { return @"validation"; } }
-        public static string ResetPassword { get { return @"resetpassword"; } }
-        public static string ChangeEmail { get { return @"changeemail"; } }
-        public static string ChangePassword { get { return @"changepassword"; } }
     }
 
     public class CartActionNames
@@ -38,10 +32,5 @@
         public static string Index { get { return @""; } }
         public static string Article { get { return @"article"; } }
         public static string AddToCart { get { return @"addtocart"; } }
-    }
-
-    public class FeedbackActionNames
-    {
-        public static string Index { get { return @""; } }
     }
 }
--- src/phiNdus.fundus.Web/Controllers/AccountController.cs
+++ src/phiNdus.fundus.Web/Controllers/AccountController.cs
@@ -1,21 +1,14 @@
 ﻿namespace Phundus.Web.Controllers
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
     using System.Web.Mvc;
     using Castle.Transactions;
-    using Core.IdentityAndAccess.Organizations.Commands;
     using Core.IdentityAndAccess.Queries;
-    using Core.IdentityAndAccess.Users.Commands;
     using Core.IdentityAndAccess.Users.Exceptions;
-    using Core.IdentityAndAccess.Users.Mails;
     using Core.IdentityAndAccess.Users.Repositories;
     using Infrastructure.Gateways;
-    using phiNdus.fundus.Web.Models;
     using phiNdus.fundus.Web.Security;
     using phiNdus.fundus.Web.ViewModels;
-    using phiNdus.fundus.Web.ViewModels.Account;
 
     public class AccountController : ControllerBase
     {
@@ -76,76 +69,6 @@
                 }
             }
             return View(model);
-        }
-
-        [Transaction]
-        public virtual ActionResult ChangeEmail()
-        {
-            return View(new ChangeEmailViewModel());
-        }
-
-        [HttpPost]
-        [Transaction]
-        public virtual ActionResult ChangeEmail(ChangeEmailViewModel model)
-        {
-            if (!ModelState.IsValid)
-            {
-                ModelState.AddModelError("", "Eines oder mehrere Felder enthalten ungültige Werte.");
-                return View(model);
-            }
-
-            try
-            {
-                try
-                {
-                    if (MembershipProvider.ChangeEmail(Identity.Name, model.Email))
-                        return View("ChangeEmailDone");
-                    ModelState.AddModelError("", "Unbekannter Fehler beim Ändern der E-Mail-Adresse.");
-                }
-                catch (EmailAlreadyTakenException)
-                {
-                    ModelState.AddModelError("", "Die E-Mail-Adresse wird bereits verwendet.");
-                }
-
-
-                return View(model);
-            }
-            catch (Exception ex)
-            {
-                ModelState.AddModelError("", ex.Message);
-                return View(model);
-            }
-        }
-
-        [Transaction]
-        public virtual ActionResult ChangePassword()
-        {
-            return View(new ChangePasswordViewModel());
-        }
-
-        [HttpPost]
-        [Transaction]
-        public virtual ActionResult ChangePassword(ChangePasswordViewModel model)
-        {
-            if (!ModelState.IsValid)
-            {
-                ModelState.AddModelError("", "Eines oder mehrere Felder enthalten ungültige Werte.");
-                return View(model);
-            }
-
-            try
-            {
-                if (MembershipProvider.ChangePassword(Identity.Name, model.OldPassword, model.NewPassword))
-                    return View("ChangePasswordDone");
-
-                ModelState.AddModelError("", "Unbekannter Fehler beim Ändern des Passwortes.");
-                return View(model);
-            }
-            catch (Exception ex)
-            {
-                ModelState.AddModelError("", ex.Message);
-                return View(model);
-            }
         }
     }
 }
