535790d536cae887ccc3a75248f089f3b64746cd
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2016-01-10 09:18:27 +0000

    Debug-Output.

--- trunk/specs/Phundus.Specs/Services/Ctx.cs
+++ trunk/specs/Phundus.Specs/Services/Ctx.cs
@@ -2,15 +2,54 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics;
     using Entities;
     using TechTalk.SpecFlow;
+
+    public class Aliases<T>
+    {
+        private readonly IDictionary<string, T> _collection = new Dictionary<string, T>(); 
+        
+        public T this[string alias]
+        {
+            get
+            {
+                if (alias == null)
+                    return default(T);
+                T result;
+                if (!_collection.TryGetValue(alias, out result))
+                    throw new InvalidOperationException(String.Format("Alias {0} unknown.", alias));
+                return result;
+            }
+            set
+            {
+                Debug.WriteLine("Aliased {0} to {1}.", alias, value.ToString());
+                _collection[alias] = value;
+            }
+        }
+
+        public void Clear()
+        {
+            _collection.Clear();
+        }
+
+        public bool ContainsAlias(string alias)
+        {
+            return _collection.ContainsKey(alias);
+        }
+
+        public void TryGetValue(string alias, out T value)
+        {
+            _collection.TryGetValue(alias, out value);
+        }
+    }
 
     [Binding]
     public class Ctx
     {
-        private readonly IDictionary<string, string> _emails = new Dictionary<string, string>();
-        private readonly IDictionary<string, User> _users = new Dictionary<string, User>();
-
+        private readonly Aliases<string> _emails = new Aliases<string>();
+        private readonly Aliases<User> _users = new Aliases<User>();
+            
         [BeforeScenario]
         public void BeforeScenario()
         {
@@ -25,9 +64,14 @@
 
         public User User { get; set; }
 
-        public IDictionary<string, User> Users
+        public Aliases<User> Users
         {
             get { return _users; }
+        }
+
+        public Aliases<string> Emails
+        {
+            get { return _emails; }
         }
 
         public Organization Organization { get; set; }
@@ -40,10 +84,5 @@
         public Guid LoggedIn { get; set; }
 
         public string ValidationKey { get; set; }
-
-        public IDictionary<string, string> Emails
-        {
-            get { return _emails; }
-        }
     }
 }
