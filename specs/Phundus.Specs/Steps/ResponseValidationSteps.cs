f11b505e75b809e824fca7a13b3b2456f695867d
Reto Inderbitzin <mail@indr.ch>
2016-01-11 07:51:51 +0000

    Query mit Parameters.

--- specs/Phundus.Specs/Steps/ResponseValidationSteps.cs
+++ specs/Phundus.Specs/Steps/ResponseValidationSteps.cs
@@ -1,5 +1,6 @@
 ﻿namespace Phundus.Specs.Steps
 {
+    using System.Net;
     using NUnit.Framework;
     using Services;
     using TechTalk.SpecFlow;
@@ -15,6 +16,18 @@
         public void ThenResponse(int statusCode)
         {
             Assert.That((int)App.LastResponse.StatusCode, Is.EqualTo(statusCode));
+        }
+
+        [Then(@"I should see error")]
+        public void ThenIShouldSeeError()
+        {
+            Assert.That(App.LastResponse.StatusCode, Is.EqualTo(HttpStatusCode.InternalServerError));
+        }
+
+        [Then(@"I should see ok")]
+        public void ThenIShouldSeeOk()
+        {
+            Assert.That(App.LastResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
         }
 
     }
