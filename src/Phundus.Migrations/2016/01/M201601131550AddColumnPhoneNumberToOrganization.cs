cd7b08e8ab9edcf644ecd52eb5aebe311e5e968b
Reto Inderbitzin <mail@indr.ch>
2016-01-13 15:18:06 +0000

    LessorQueries via View_Shop_Lessors.

---
+++ src/Phundus.Migrations/2016/01/M201601131550AddColumnPhoneNumberToOrganization.cs
@@ -0,0 +1,19 @@
+﻿namespace Phundus.Migrations
+{
+    using System;
+    using FluentMigrator;
+
+    [Migration(201601131550)]
+    public class M201601131550AddColumnPhoneNumberToOrganization : MigrationBase
+    {
+        public override void Up()
+        {
+            Alter.Table("Dm_IdentityAccess_Organization").AddColumn("PhoneNumber").AsString().Nullable();
+        }
+
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
