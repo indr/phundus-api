54eb9390c38e494edc061b37481f343a8d84111e
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2016-01-14 05:49:24 +0000

    EstablishOrganizationHandlerSpecs

---
+++ trunk/tests/Phundus.Core.Tests/IdentityAccess/Organizations/Commands/EstablishOrganizationHandlerSpecs.cs
@@ -0,0 +1,46 @@
+﻿namespace Phundus.Core.Tests.IdentityAccess.Organizations.Commands
+{
+    using System;
+    using Core.IdentityAndAccess.Organizations.Commands;
+    using Core.IdentityAndAccess.Organizations.Model;
+    using Core.IdentityAndAccess.Organizations.Repositories;
+    using Core.IdentityAndAccess.Users.Model;
+    using Core.Inventory.Stores.Repositories;
+    using Machine.Fakes;
+    using Machine.Specifications;
+    using Phundus.IdentityAccess.Users.Repositories;
+    using Rhino.Mocks;
+
+    [Subject(typeof (EstablishOrganizationHandler))]
+    public class when_handled : handler_concern<EstablishOrganization, EstablishOrganizationHandler>
+    {
+        private static int theInititatorId = 1001;
+        private static Guid theOrganizationGuid = Guid.NewGuid();
+        private static IOrganizationRepository organizationRepository;
+        private static IStoreRepository storeRepository;
+
+        private Establish ctx = () =>
+        {
+            organizationRepository = depends.on<IOrganizationRepository>();
+            depends.on<IUserRepository>().WhenToldTo(x => x.GetById(theInititatorId)).Return(CreateAdmin(theInititatorId));
+            command = new EstablishOrganization
+            {
+                InitiatorId = theInititatorId,
+                Name = "New Organization",
+                OrganizationId = theOrganizationGuid
+            };
+        };
+
+        private It should_add_to_organization_repository =
+            () => organizationRepository.WasToldTo(x => x.Add(Arg<Organization>.Is.NotNull));
+
+        private It should_publish_membership_application_filed =
+            () => publisher.WasToldTo(x => x.Publish(Arg<MembershipApplicationFiled>.Is.NotNull));
+
+        private It should_publish_organization_established =
+            () => publisher.WasToldTo(x => x.Publish(Arg<OrganizationEstablished>.Is.NotNull));
+
+        private It should_publish_membership_application_approved =
+            () => publisher.WasToldTo(x => x.Publish(Arg<MembershipApplicationApproved>.Is.NotNull));
+    }
+}
--- trunk/tests/Phundus.Core.Tests/handler_concern.cs
+++ trunk/tests/Phundus.Core.Tests/handler_concern.cs
@@ -1,7 +1,10 @@
 namespace Phundus.Core.Tests
 {
+    using Common.Domain.Model;
     using Core.Cqrs;
+    using Core.IdentityAndAccess.Users.Model;
     using Machine.Specifications;
+    using NUnit.Framework;
 
     public abstract class handler_concern<TCommand, THandler> : concern<THandler>
         where THandler : class, IHandleCommand<TCommand>
@@ -13,5 +16,23 @@ namespace Phundus.Core.Tests
             command.ShouldNotBeNull();
             sut.Handle(command);
         };
+
+        protected static User CreateAdmin(int userId)
+        {
+            return CreateAdmin(new UserId(userId));
+        }
+
+        protected static User CreateAdmin(UserId userId)
+        {
+            var result = new User("admin@test.phundus.ch", "1234", "Bob", "Cooper", "Street", "1000", "City",
+                "012 345 67 89", null);
+            var property = typeof (User).GetProperty("Id");
+            Assert.That(property, Is.Not.Null, "Could not find property Id of type {0}. " +
+                                               "If User is no longer of base type EntityBase and has no database generated numeric identifier, remove this hack.",
+                typeof (User));
+            property.GetSetMethod(true).Invoke(result, new object[] {userId.Id});
+
+            return result;
+        }
     }
 }
