ec062a70d864b53d5ace191fc569e23b24ccf6e0
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2016-01-14 16:25:20 +0000

    Added Period.

---
+++ trunk/tests/Phundus.Common.Tests/Domain.Model/PeriodSpecs.cs
@@ -0,0 +1,58 @@
+﻿namespace Phundus.Common.Tests.Domain.Model
+{
+    using System;
+    using Common.Domain.Model;
+    using Machine.Specifications;
+
+    public class period_concern
+    {
+        protected static DateTime theFromUtc = new DateTime(2010, 11, 1);
+        protected static DateTime theToUtc = new DateTime(2010, 11, 2);
+
+        protected static Period sut;
+    }
+
+    [Subject(typeof (Period))]
+    public class when_creating_a_period : period_concern
+    {
+        private Because of = () => sut = new Period(theFromUtc, theToUtc);
+
+        private It should_have_from_utc = () => sut.FromUtc.ShouldEqual(theFromUtc);
+
+        private It should_have_to_utc = () => sut.ToUtc.ShouldEqual(theToUtc);
+    }
+
+    [Subject(typeof (Period))]
+    public class when_creating_a_period_with_smaller_from_than_to : period_concern
+    {
+        private static Exception caught;
+
+        private Establish ctx = () =>
+        {
+            theFromUtc = DateTime.Now.AddDays(1);
+            theToUtc = DateTime.Now.AddDays(-1);
+        };
+
+        private Because of = () => caught = Catch.Exception(() => sut = new Period(theFromUtc, theToUtc));
+
+        private It should_throw_argument_exception = () => caught.ShouldBeOfExactType<ArgumentException>();
+
+        private It should_throw_exception_with_message =
+            () => caught.Message.ShouldEqual("The from date must be less or equal the to date.");
+    }
+
+    [Subject(typeof (Period))]
+    public class when_comparing_two_periods_with_same_values : period_concern
+    {
+        private static Period theOtherPeriod;
+        private static bool result;
+        private Establish ctx = () =>
+        {
+            sut = new Period(theFromUtc, theToUtc);
+            theOtherPeriod = new Period(theFromUtc, theToUtc);
+        };
+        private Because of = () => result = Equals(sut, theOtherPeriod);
+
+        private It should_be_equal = () => result.ShouldBeTrue();
+    }
+}
