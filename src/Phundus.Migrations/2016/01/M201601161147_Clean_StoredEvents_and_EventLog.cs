d70056877dcf68f347c4bf7a5750b65fc11b9c68
Reto Inderbitzin <mail@indr.ch>
2016-01-16 10:50:46 +0000

    StoredEvents und EventLog leeren.

---
+++ src/Phundus.Migrations/2016/01/M201601161147_Clean_StoredEvents_and_EventLog.cs
@@ -0,0 +1,20 @@
+namespace Phundus.Migrations
+{
+    using System;
+    using FluentMigrator;
+
+    [Migration(201601161147)]
+    public class M201601161147_Clean_StoredEvents_and_EventLog : MigrationBase
+    {
+        public override void Up()
+        {
+            Delete.FromTable("Rm_EventLog").AllRows();
+            Delete.FromTable("StoredEvents").AllRows();
+        }
+
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
