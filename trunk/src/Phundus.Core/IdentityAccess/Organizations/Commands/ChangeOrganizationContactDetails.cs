1c567f1bdcc0fb45758818bad2f646f884da96a4
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2016-01-17 19:48:56 +0000

    UpdateOrganizationDetails in ChangeOrganizationContactDetails umbennant.

---
+++ trunk/src/Phundus.Core/IdentityAccess/Organizations/Commands/ChangeOrganizationContactDetails.cs
@@ -0,0 +1,52 @@
+﻿namespace Phundus.IdentityAccess.Organizations.Commands
+{
+    using System;
+    using Common.Domain.Model;
+    using Cqrs;
+    using Ddd;
+    using Model;
+    using Queries;
+    using Repositories;
+
+    public class ChangeOrganizationContactDetails : ICommand
+    {
+        public ChangeOrganizationContactDetails(InitiatorGuid initiatorId, OrganizationGuid organizationId, string postAddress,
+            string phoneNumber, string emailAddress, string website)
+        {
+            if (initiatorId == null) throw new ArgumentNullException("initiatorId");
+            if (organizationId == null) throw new ArgumentNullException("organizationId");
+            InitiatorId = initiatorId;
+            OrganizationId = organizationId;
+            PostAddress = postAddress;
+            PhoneNumber = phoneNumber;
+            EmailAddress = emailAddress;
+            Website = website;
+        }
+
+        public InitiatorGuid InitiatorId { get; protected set; }
+        public OrganizationGuid OrganizationId { get; protected set; }
+        public string PostAddress { get; protected set; }
+        public string PhoneNumber { get; protected set; }
+        public string EmailAddress { get; protected set; }
+        public string Website { get; protected set; }
+    }
+
+    public class ChangeOrganizationContactDetailsHandler : IHandleCommand<ChangeOrganizationContactDetails>
+    {
+        public IMemberInRole MemberInRole { get; set; }
+
+        public IOrganizationRepository Repository { get; set; }
+
+        public void Handle(ChangeOrganizationContactDetails command)
+        {
+            MemberInRole.ActiveChief(command.OrganizationId.Id, command.InitiatorId);
+
+            var organization = Repository.GetById(command.OrganizationId.Id);
+
+            organization.ChangeContactDetails(new ContactDetails(command.PostAddress, command.PhoneNumber,
+                command.EmailAddress, command.Website));
+
+            EventPublisher.Publish(new OrganizationUpdated());
+        }
+    }
+}
---
+++ trunk/tests/Phundus.Core.Tests/IdentityAccess/identityaccess_factory.cs
@@ -0,0 +1,21 @@
+﻿namespace Phundus.Tests.IdentityAccess
+{
+    using Common.Domain.Model;
+    using developwithpassion.specifications.core;
+    using developwithpassion.specifications.extensions;
+    using Phundus.IdentityAccess.Organizations.Model;
+
+    public class identityaccess_factory : factory_base
+    {
+        public identityaccess_factory(ICreateFakes fake) : base(fake)
+        {
+        }
+
+        public Organization Organization()
+        {
+            var result = fake.an<Organization>();
+            result.setup(x => x.Id).Return(new OrganizationGuid().Id);
+            return result;
+        }
+    }
+}
