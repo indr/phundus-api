3a942e45ad239ab3c78acc928ea0cb81f2502d68
Reto Inderbitzin <mail@indr.ch>
2016-01-18 21:15:04 +0000

    Mal schauen..

--- src/Phundus.Migrations/2016/01/M201601182024_Add_UserGuid_and_OrganizationGuid_to_membership_application_events.cs
+++ src/Phundus.Migrations/2016/01/M201601182024_Add_UserGuid_and_OrganizationGuid_to_membership_application_events.cs
@@ -2,30 +2,42 @@ namespace Phundus.Migrations
 {
     using System;
     using System.Runtime.Serialization;
+    using System.Text;
     using Common.Domain.Model;
     using FluentMigrator;
 
     [Migration(201601182024)]
     public class M201601182024_Add_UserGuid_and_OrganizationGuid_to_membership_application_events : EventMigrationBase
     {
+        private StringBuilder _sb;
+
         protected override void Migrate()
         {
+            _sb = new StringBuilder();
             Process(@"Phundus.IdentityAccess.Organizations.Model.MembershipApplicationFiled, Phundus.Core");
             Process(@"Phundus.IdentityAccess.Organizations.Model.MembershipApplicationRejected, Phundus.Core");
             Process(@"Phundus.IdentityAccess.Organizations.Model.MembershipApplicationApproved, Phundus.Core");
+            throw new Exception(_sb.ToString());
         }
 
         private void Process(string typeName)
         {
+            
             ForEach<MembershipApplicationEvent>(typeName,
                 (eventId, domainEvent) =>
                 {
                     if ((domainEvent.OrganizationGuid != Guid.Empty) && (domainEvent.UserGuid != Guid.Empty))
                         return;
 
-                    domainEvent.OrganizationGuid = GetOrganizationGuid(domainEvent.OrganizationIntegralId);
-                    domainEvent.UserGuid = GetUserGuid(domainEvent.UserIntegralId);
-                    UpdateSerialization(eventId, domainEvent);
+                    //domainEvent.OrganizationGuid = GetOrganizationGuid(domainEvent.OrganizationIntegralId);
+                    //domainEvent.UserGuid = GetUserGuid(domainEvent.UserIntegralId);
+                    //UpdateSerialization(eventId, domainEvent);
+
+                    if (OrganizationIdMap.ContainsKey(domainEvent.OrganizationIntegralId))
+                        return;
+
+                    _sb.AppendLine(String.Format("Org: {0}, User {1}", domainEvent.OrganizationIntegralId,
+                        domainEvent.UserIntegralId));
                 });
         }
 
--- src/Phundus.Migrations/2016/01/M201601182104_Reset_all_ProcessedNotificationTracker.cs
+++ src/Phundus.Migrations/2016/01/M201601182104_Reset_all_ProcessedNotificationTracker.cs
@@ -3,12 +3,11 @@ namespace Phundus.Migrations
     using System;
     using FluentMigrator;
 
-    [Migration(201601182104)]
     public class M201601182104_Reset_all_ProcessedNotificationTracker : MigrationBase
     {
         public override void Up()
         {
-            Delete.FromTable("ProcessedNotificationTracker").AllRows();
+            //Delete.FromTable("ProcessedNotificationTracker").AllRows();
         }
 
         public override void Down()
