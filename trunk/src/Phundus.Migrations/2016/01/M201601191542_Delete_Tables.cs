479ab9f378df8d6d0c2201f8215dd7c8968f10d5
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2016-01-19 14:48:33 +0000

    Delete unused tables.

---
+++ trunk/src/Phundus.Migrations/2016/01/M201601191542_Delete_Tables.cs
@@ -0,0 +1,25 @@
+﻿namespace Phundus.Migrations
+{
+    using System;
+    using FluentMigrator;
+
+    [Migration(201601191542)]
+    public class M201601191542_Delete_Unused_Tables : MigrationBase
+    {
+        public override void Up()
+        {
+            Delete.Table("Rm_Relationships");
+            Rename.Table("Rm_EventLog").To("Es_Dashboard_EventLog");
+
+            if (Schema.Table("model_store").Exists())
+                Delete.Table("model_store");
+            if (Schema.Table("SequelizeMeta").Exists())
+                Delete.Table("SequelizeMeta");
+        }
+
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
