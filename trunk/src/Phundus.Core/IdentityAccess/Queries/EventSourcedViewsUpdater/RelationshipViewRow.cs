7c30fccc3c2235af815941648795fd046363d61d
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2016-01-19 07:27:11 +0000

    SaveOrUpdate().

---
+++ trunk/src/Phundus.Core/IdentityAccess/Queries/EventSourcedViewsUpdater/RelationshipViewRow.cs
@@ -0,0 +1,17 @@
+﻿namespace Phundus.IdentityAccess.Queries.EventSourcedViewsUpdater
+{
+    using System;
+
+    public class RelationshipViewRow
+    {
+        public virtual Guid RelationshipGuid { get; set; }
+
+        public virtual Guid OrganizationGuid { get; set; }
+
+        public virtual Guid UserGuid { get; set; }
+
+        public virtual DateTime Timestamp { get; set; }
+
+        public virtual string Status { get; set; }
+    }
+}
