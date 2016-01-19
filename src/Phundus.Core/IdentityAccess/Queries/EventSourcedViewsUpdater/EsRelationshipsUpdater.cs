8159855df76a72824b790c562932949565345b99
Reto Inderbitzin <mail@indr.ch>
2016-01-19 07:27:11 +0000

    SaveOrUpdate().

--- src/Phundus.Core/IdentityAccess/Queries/EventSourcedViewsUpdater/EsRelationshipsUpdater.cs
+++ src/Phundus.Core/IdentityAccess/Queries/EventSourcedViewsUpdater/EsRelationshipsUpdater.cs
@@ -5,26 +5,6 @@
     using Common.Notifications;
     using Cqrs;
     using Organizations.Model;
-    using ReadModels;
-
-    public class RelationshipViewRow
-    {
-        private Guid _relationshipGuid = Guid.NewGuid();
-
-        public virtual Guid RelationshipGuid
-        {
-            get { return _relationshipGuid; }
-            set { _relationshipGuid = value; }
-        }
-
-        public virtual Guid OrganizationGuid { get; set; }
-
-        public virtual Guid UserGuid { get; set; }
-
-        public virtual DateTime Timestamp { get; set; }
-
-        public virtual string Status { get; set; }
-    }
 
     public class EsRelationshipsUpdater : NHibernateReadModelBase<RelationshipViewRow>, IDomainEventHandler
     {
@@ -40,44 +20,37 @@
 
         public void Process(MembershipApplicationFiled domainEvent)
         {
-            UpdateOrInsert(domainEvent.UserGuid, domainEvent.OrganizationGuid, domainEvent.OccuredOnUtc,
-                RelationshipStatusDto.Application);
+            UpdateOrInsert(domainEvent.UserGuid, domainEvent.OrganizationGuid, domainEvent.OccuredOnUtc, "application");
         }
 
         public void Process(MembershipApplicationApproved domainEvent)
         {
-            UpdateOrInsert(domainEvent.UserGuid, domainEvent.OrganizationGuid, domainEvent.OccuredOnUtc,
-                RelationshipStatusDto.Member);
+            UpdateOrInsert(domainEvent.UserGuid, domainEvent.OrganizationGuid, domainEvent.OccuredOnUtc, "member");
         }
 
         public void Process(MembershipApplicationRejected domainEvent)
         {
-            UpdateOrInsert(domainEvent.UserGuid, domainEvent.OrganizationGuid, domainEvent.OccuredOnUtc,
-                RelationshipStatusDto.Rejected);
+            UpdateOrInsert(domainEvent.UserGuid, domainEvent.OrganizationGuid, domainEvent.OccuredOnUtc, "rejected");
         }
 
-        private void UpdateOrInsert(Guid userId, Guid organizationId, DateTime timestamp, RelationshipStatusDto status)
+        private void UpdateOrInsert(Guid userId, Guid organizationId, DateTime timestamp, string status)
         {
-            var row = QueryOver().Where(p => 
-                    p.UserGuid == userId && p.OrganizationGuid == organizationId).SingleOrDefault();
+            var row = QueryOver().Where(p =>
+                p.UserGuid == userId && p.OrganizationGuid == organizationId).SingleOrDefault();
+            
             if (row == null)
             {
                 row = new RelationshipViewRow
                 {
                     OrganizationGuid = organizationId,
-                    UserGuid = userId,
-                    Timestamp = timestamp,
-                    Status = status.ToString()
+                    UserGuid = userId
                 };
-                Session.Save(row);
             }
-            else
-            {
-                row.Status = status.ToString();
-                row.Timestamp = timestamp;
 
-                Session.SaveOrUpdate(row);
-            }
+            row.Status = status;
+            row.Timestamp = timestamp;
+
+            Session.SaveOrUpdate(row);
         }
     }
 }
