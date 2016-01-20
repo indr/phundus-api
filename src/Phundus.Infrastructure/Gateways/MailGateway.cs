0b136c0cbf4acc8513490a250617adbcae08b031
Reto Inderbitzin <mail@indr.ch>
2016-01-20 08:12:08 +0000

    MailGateway always stores sent message to default pickup directory.

--- src/Phundus.Infrastructure/Gateways/MailGateway.cs
+++ src/Phundus.Infrastructure/Gateways/MailGateway.cs
@@ -6,8 +6,6 @@
 
     public class MailGateway : IMailGateway
     {
-        #region IMailGateway Members
-
         public void Send(string recipients, string subject, string body)
         {
             if (recipients == null) throw new ArgumentNullException("recipients");
@@ -21,24 +19,47 @@
 
         public void Send(MailMessage message)
         {
+            SendUsingSystemNetMailSettings(message);
+            SaveToPickupDirectory(message);
+        }
+
+        private static void SendUsingSystemNetMailSettings(MailMessage message)
+        {
             var client = GetClient();
+            client.Send(message);
+        }
+
+        private static void SaveToPickupDirectory(MailMessage message)
+        {
+            var defaultPickupDirectoryLocation = MapPath(@"~\App_Data\Mails");
+
+            // If the system.net.mail.settings is equal to our default pickup directory location, prevent sending / storing
+            if (GetClient().PickupDirectoryLocation == defaultPickupDirectoryLocation)
+                return;
+
+            var client = new SmtpClient("", 0);
+            client.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
+            client.PickupDirectoryLocation = defaultPickupDirectoryLocation;
             client.Send(message);
         }
 
         private static SmtpClient GetClient()
         {
             var client = new SmtpClient();
-            if (!String.IsNullOrWhiteSpace(client.PickupDirectoryLocation))
+            if (String.IsNullOrWhiteSpace(client.PickupDirectoryLocation))
+                return client;
+
+            if (!System.IO.Path.IsPathRooted(client.PickupDirectoryLocation))
             {
-                if (!System.IO.Path.IsPathRooted(client.PickupDirectoryLocation))
-                {
-                    var path = HttpContext.Current.Server.MapPath(client.PickupDirectoryLocation);
-                    client.PickupDirectoryLocation = path;
-                }
+                var path = MapPath(client.PickupDirectoryLocation);
+                client.PickupDirectoryLocation = path;
             }
             return client;
         }
 
-        #endregion
+        private static string MapPath(string path)
+        {
+            return HttpContext.Current.Server.MapPath(path);
+        }
     }
 }
