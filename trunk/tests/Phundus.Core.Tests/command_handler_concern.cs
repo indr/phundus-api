612557c8207060172d377fef261423775e7b4d44
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2016-02-02 10:10:38 +0000

    command_handler_concern with EnforcedInitiatorTo<>.

--- trunk/tests/Phundus.Core.Tests/command_handler_concern.cs
+++ trunk/tests/Phundus.Core.Tests/command_handler_concern.cs
@@ -1,11 +1,14 @@
 namespace Phundus.Tests
 {
+    using System;
+    using System.Linq.Expressions;
     using Authorization;
     using Common.Domain.Model;
     using Integration.IdentityAccess;
     using Machine.Fakes;
     using Machine.Specifications;
     using Phundus.Cqrs;
+    using Rhino.Mocks;
 
     public abstract class command_handler_concern<TCommand, THandler> : concern<THandler>
         where THandler : class, IHandleCommand<TCommand>
@@ -35,5 +38,12 @@ namespace Phundus.Tests
             else
                 sut.Handle(command);
         };
+
+        protected static IMethodCallOccurrence EnforcedInitiatorTo<T>(Expression<Predicate<T>> accessObjectPredicate)
+        {
+            return authorize.WasToldTo(x =>
+                x.Enforce(Arg<InitiatorId>.Is.Equal(theInitiatorId),
+                    Arg<T>.Matches(accessObjectPredicate)));
+        }
     }
 }
