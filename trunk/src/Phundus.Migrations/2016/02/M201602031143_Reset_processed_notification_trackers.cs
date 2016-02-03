cd9cb7b3320a6b862d3dcbf46e3b9112bf2e743e
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2016-02-03 10:47:17 +0000

    Reset all processed notification trackers.

---
+++ trunk/src/Phundus.Migrations/2016/02/M201602031143_Reset_processed_notification_trackers.cs
@@ -0,0 +1,22 @@
+namespace Phundus.Migrations
+{
+    using System;
+    using FluentMigrator;
+
+    [Migration(201602031143)]
+    public class M201602031143_Reset_processed_notification_trackers : MigrationBase
+    {
+        public override void Up()
+        {
+            ResetAllProcessedNotificationTrackers();
+            Delete.FromTable("Es_Dashboard_EventLog").AllRows();
+            Delete.FromTable("Es_IdentityAccess_Relationships").AllRows();
+            Delete.FromTable("Es_Inventory_Articles").AllRows();
+        }
+
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
