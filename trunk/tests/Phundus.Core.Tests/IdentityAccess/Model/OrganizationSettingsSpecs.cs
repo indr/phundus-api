467cc31fac13fbe71a3a91055da4deeb03351bcc
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2016-02-09 09:30:21 +0000

    Public rental defaults to True.

--- trunk/tests/Phundus.Core.Tests/IdentityAccess/Model/OrganizationSettingsSpecs.cs
+++ trunk/tests/Phundus.Core.Tests/IdentityAccess/Model/OrganizationSettingsSpecs.cs
@@ -10,18 +10,21 @@
     [Subject(typeof (Settings))]
     public class when_instantiating_settings : Observes<Settings>
     {
+        private Establish ctx = () =>
+            sut_factory.create_using(() => new Settings());
+
         private It should_have_public_rental_false = () =>
-            sut.PublicRental.ShouldBeFalse();
+            sut.PublicRental.ShouldBeTrue();
     }
 
     [Subject(typeof(Organization))]
-    public class when_changing_setting_public_rental_to_true : organization_concern
+    public class when_changing_setting_public_rental_to_false : organization_concern
     {
         private Because of = () =>
-            sut.ChangeSettingPublicRental(theInitiator, true);
+            sut.ChangeSettingPublicRental(theInitiator, false);
 
         private It should_have_setting_public_rental_true = () =>
-            sut.Settings.PublicRental.ShouldBeTrue();
+            sut.Settings.PublicRental.ShouldBeFalse();
 
         private It should_publish_public_rental_setting_changed = () =>
             publisher.WasToldTo(x => x.Publish(Arg<PublicRentalSettingChanged>.Is.NotNull));
