bb4b6b7b2041b0b317f56a00aca05f223ca309a9
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2016-02-11 03:18:37 +0000

    Alter Shop entfernt!

--- trunk/src/Phundus.Core/Cqrs/AutoMappingReadModelBase.cs
+++ trunk/src/Phundus.Core/Cqrs/AutoMappingReadModelBase.cs
@@ -5,7 +5,6 @@ namespace Phundus.Cqrs
     using System.Data;
     using System.Linq;
     using AutoMapper;
-    using Paging;
 
     public abstract class AutoMappingReadModelBase : ReadModelBase
     {
@@ -42,33 +41,6 @@ namespace Phundus.Cqrs
         public ICollection<T> Many<T>(string sql, object arg0)
         {
             return Many<T>(String.Format(sql, arg0));
-        }
-
-        public PagedResult<T> Paged<T>(string sql, PageRequest page)
-        {
-            if (!sql.StartsWith("select ", StringComparison.InvariantCultureIgnoreCase))
-                throw new ArgumentException("Select command must start with the keyword select");
-
-            var total = GetTotal(sql);
-
-            // TODO: SQL Server 2012 OFFSET and FETCH
-            //sql = sql + String.Format(" offset {0} rows fetch next {1} rows only", page.Offset, page.Size);
-
-            return new PagedResult<T>(PageResponse.From(page, total),
-                Many<T>(sql).Skip(page.Offset).Take(page.Size).ToList());
-        }
-
-        private int GetTotal(string sql)
-        {
-            var orderByIndex = sql.IndexOf(" order by ", StringComparison.InvariantCultureIgnoreCase);
-            if (orderByIndex >= 0)
-                sql = sql.Remove(orderByIndex);
-
-            var fromIndex = sql.IndexOf(" from ", StringComparison.InvariantCultureIgnoreCase);
-
-            sql = "select count(*) " + sql.Remove(0, fromIndex);
-
-            return ExecuteScalar<int>(sql);
         }
     }
 }
