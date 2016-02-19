e7f3dd424de698093fa7e60138eb25dc5ed39ab3
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2016-02-19 10:56:30 +0000

    OrderPlaced with Items.

--- trunk/tests/Phundus.Core.Tests/Shop/Events/OrderPlacedSpecs.cs
+++ trunk/tests/Phundus.Core.Tests/Shop/Events/OrderPlacedSpecs.cs
@@ -1,5 +1,7 @@
 ﻿namespace Phundus.Tests.Shop.Events
 {
+    using System;
+    using System.Collections.Generic;
     using Common.Domain.Model;
     using Machine.Specifications;
     using Phundus.Shop.Model;
@@ -12,20 +14,24 @@
         private static Lessor theLessor;
         private static OrderId theOrderId;
         private static Lessee theLessee;
+        private static int theStatus = 1;
+        private static decimal theTotalPrice = 10.20m;
+        private static List<OrderPlaced.Item> theItems;
 
         private Establish ctx = () =>
         {
-            var make = new shop_factory(fake);
-
             theShortOrderId = new ShortOrderId(1234);
             theOrderId = new OrderId();
             theLessor = new Lessor(new LessorId(), "The lessor", true);
             theLessee = new Lessee(new LesseeId(), "First name", "Last name", "Street", "Postcode", "City",
                 "Email address", "Phone number", "Member number");
+            theItems = new List<OrderPlaced.Item>();
+            theItems.Add(new OrderPlaced.Item(Guid.NewGuid(), new ArticleId(), new ArticleShortId(1234),
+                "The text", 1.23m, DateTime.Today, DateTime.Today.AddDays(1), 10, 12.3m));
 
             sut_factory.create_using(() =>
                 new OrderPlaced(theInitiator, theOrderId, theShortOrderId,
-                    theLessor, theLessee));
+                    theLessor, theLessee, theStatus, theTotalPrice, theItems));
         };
 
         private It should_be_in_assembly = () =>
@@ -49,8 +55,68 @@
         private It should_have_at_6_the_lessee = () =>
             dataMember(6).ShouldEqual(theLessee);
 
+        private It should_have_at_7_the_status = () =>
+            dataMember(7).ShouldEqual(theStatus);
+
+        private It should_have_at_8_the_total_price = () =>
+            dataMember(8).ShouldEqual(theTotalPrice);
+
+        private It should_have_at_9_the_items = () =>
+            dataMember(9).ShouldBeLike(theItems);
+
         private It should_have_full_name = () =>
             itsFullName.ShouldEqual("Phundus.Shop.Orders.Model.OrderPlaced");
+    }
 
+    [Subject(typeof (OrderPlaced.Item))]
+    public class order_placed_item : domain_event_concern<OrderPlaced.Item>
+    {
+        private static Guid theItemId = Guid.NewGuid();
+        private static ArticleId theArticleId = new ArticleId();
+        private static ArticleShortId theArticleShortId = new ArticleShortId(1234);
+        private static string theText = "The text";
+        private static decimal theUnitPricePerWeek = 1.23m;
+        private static DateTime theFromUtc = DateTime.Today;
+        private static DateTime theToUtc = DateTime.Today.AddDays(1);
+        private static int theQuantity = 2;
+        private static decimal theItemTotal = 2.50m;
+
+        private Establish ctx = () => sut_factory.create_using(() =>
+            new OrderPlaced.Item(theItemId, theArticleId, theArticleShortId,
+                theText, theUnitPricePerWeek, theFromUtc, theToUtc, theQuantity,
+                theItemTotal));
+
+        private It should_be_in_assembly = () =>
+            itsAssembly.ShouldEqual("Phundus.Core");
+
+        private It should_have_at_1_the_item_id = () =>
+            dataMember(1).ShouldEqual(theItemId);
+
+        private It should_have_at_2_the_article_id = () =>
+            dataMember(2).ShouldEqual(theArticleId.Id);
+
+        private It should_have_at_3_the_article_short_id = () =>
+            dataMember(3).ShouldEqual(theArticleShortId.Id);
+
+        private It should_have_at_4_the_text = () =>
+            dataMember(4).ShouldEqual(theText);
+
+        private It should_have_at_5_the_unit_price_per_week = () =>
+            dataMember(5).ShouldEqual(theUnitPricePerWeek);
+
+        private It should_have_at_6_the_from_utc = () =>
+            dataMember(6).ShouldEqual(theFromUtc);
+
+        private It should_have_at_7_the_to_utc = () =>
+            dataMember(7).ShouldEqual(theToUtc);
+
+        private It should_have_at_8_the_quantity = () =>
+            dataMember(8).ShouldEqual(theQuantity);
+
+        private It should_have_at_9_the_item_total = () =>
+            dataMember(9).ShouldEqual(theItemTotal);
+
+        private It should_have_full_name = () =>
+            itsFullName.ShouldEqual("Phundus.Shop.Orders.Model.OrderPlaced+Item");
     }
 }
