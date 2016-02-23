ccda69cacb77de5d3b6be20fac19a5a43182a5d0
Reto Inderbitzin <mail@indr.ch>
2016-02-23 04:31:09 +0000

    Fixes unit tests.

--- tests/Phundus.Core.Tests/IdentityAccess/Commands/UnlockMemberHandlerSpecs.cs
+++ tests/Phundus.Core.Tests/IdentityAccess/Commands/UnlockMemberHandlerSpecs.cs
@@ -19,6 +19,7 @@ namespace Phundus.Tests.IdentityAccess.Commands
             theOrganizationId = theOrganization.Id;
             theMemberId = new UserId();
 
+            userInRole.setup(x => x.Manager(theInitiatorId, theOrganizationId)).Return(theManager);
             organizationRepository.setup(x => x.GetById(theOrganizationId)).Return(theOrganization);
             command = new UnlockMember(theInitiatorId, theOrganizationId, theMemberId);
         };
