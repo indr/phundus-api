fd253208a2ffcc67705dc0984bdfa8ac226dcd2d
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2016-02-24 10:30:11 +0000

    Added EventSourcedRootEntity.

--- trunk/src/Phundus.Migrations/2016/02/M201602240931_Delete_Stores_StoredEvents.cs
+++ trunk/src/Phundus.Migrations/2016/02/M201602240931_Delete_Stores_StoredEvents.cs
@@ -3,7 +3,7 @@
     using System;
     using FluentMigrator;
 
-    [Migration(2016022409031)]
+    [Migration(201602240931)]
     public class M201602240931_Delete_Stores_StoredEvents : MigrationBase
     {
         public override void Up()
