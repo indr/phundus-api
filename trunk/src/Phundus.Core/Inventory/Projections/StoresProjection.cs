27bfad5aec3567c5cb4cac10b36a9bf53322194a
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2016-02-24 08:20:09 +0000

    Added StoresProjection.

---
+++ trunk/src/Phundus.Core/Inventory/Projections/StoresProjection.cs
@@ -0,0 +1,61 @@
+namespace Phundus.Inventory.Projections
+{
+    using System;
+    using Common.Domain.Model;
+    using Common.Notifications;
+    using Cqrs;
+    using Stores.Model;
+
+    public class StoresProjection : ProjectionBase<StoreRow>, IStoredEventsConsumer
+    {
+        public void Handle(DomainEvent domainEvent)
+        {
+            Process((dynamic) domainEvent);
+        }
+
+        public void Process(DomainEvent domainEvent)
+        {
+            // Noop
+        }
+
+        public void Process(StoreOpened e)
+        {
+            Insert(x =>
+            {
+                x.StoreId = e.StoreId;
+                x.OwnerId = e.Owner.OwnerId.Id;
+            });
+        }
+
+        public void Process(AddressChanged e)
+        {
+            Update(e.StoreId, x =>
+                x.Address = e.Address);
+        }
+
+        public void Process(OpeningHoursChanged e)
+        {
+            Update(e.StoreId, x =>
+                x.OpeningHours = e.OpeningHours);
+        }
+
+        public void Process(CoordinateChanged e)
+        {
+            Update(e.StoreId, x =>
+            {
+                x.Latitude = e.Latitude;
+                x.Longitude = e.Longitude;
+            });
+        }
+    }
+
+    public class StoreRow
+    {
+        public virtual Guid StoreId { get; set; }
+        public virtual Guid OwnerId { get; set; }
+        public virtual string Address { get; set; }
+        public virtual string OpeningHours { get; set; }
+        public virtual decimal? Latitude { get; set; }
+        public virtual decimal? Longitude { get; set; }
+    }
+}
---
+++ trunk/src/Phundus.Persistence/Inventory/Mappings/StoreRowMap.cs
@@ -0,0 +1,22 @@
+﻿namespace Phundus.Persistence.Inventory.Mappings
+{
+    using FluentNHibernate.Mapping;
+    using Phundus.Inventory.Projections;
+
+    public class StoreRowMap : ClassMap<StoreRow>
+    {
+        public StoreRowMap()
+        {
+            SchemaAction.All();
+
+            Table("Es_Inventory_Stores");
+
+            Id(x => x.StoreId).GeneratedBy.Assigned();
+            Map(x => x.OwnerId).Not.Nullable();
+            Map(x => x.Address).Nullable();
+            Map(x => x.OpeningHours).Nullable();
+            Map(x => x.Latitude).Nullable();
+            Map(x => x.Longitude).Nullable();
+        }
+    }
+}
