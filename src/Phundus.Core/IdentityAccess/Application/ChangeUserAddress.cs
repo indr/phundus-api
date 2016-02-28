dc9c02d84ffb6ad49b2c47889f6cf5be466f0ee6
Reto Inderbitzin <mail@indr.ch>
2016-02-28 15:09:17 +0000

    ChangeUserAddress command.

---
+++ src/Phundus.Core/IdentityAccess/Application/ChangeUserAddress.cs
@@ -0,0 +1,62 @@
+﻿namespace Phundus.IdentityAccess.Application
+{
+    using System;
+    using Common.Domain.Model;
+    using Cqrs;
+    using Integration.IdentityAccess;
+    using Users.Repositories;
+
+    public class ChangeUserAddress : ICommand
+    {
+        public ChangeUserAddress(InitiatorId initiatorId, UserId userId, string firstName, string lastName,
+            string street, string postcode, string city, string phoneNumber)
+        {
+            if (initiatorId == null) throw new ArgumentNullException("initiatorId");
+            if (userId == null) throw new ArgumentNullException("userId");
+
+            InitiatorId = initiatorId;
+            UserId = userId;
+            FirstName = firstName;
+            LastName = lastName;
+            Street = street;
+            Postcode = postcode;
+            City = city;
+            PhoneNumber = phoneNumber;
+        }
+
+        public InitiatorId InitiatorId { get; protected set; }
+        public UserId UserId { get; protected set; }
+        public string FirstName { get; protected set; }
+        public string LastName { get; protected set; }
+        public string Street { get; protected set; }
+        public string Postcode { get; protected set; }
+        public string City { get; protected set; }
+        public string PhoneNumber { get; protected set; }
+    }
+
+    public class ChangeUserAddressHandler : IHandleCommand<ChangeUserAddress>
+    {
+        private readonly IInitiatorService _initiatorService;
+        private readonly IUserRepository _userRepository;
+
+        public ChangeUserAddressHandler(IInitiatorService initiatorService, IUserRepository userRepository)
+        {
+            if (initiatorService == null) throw new ArgumentNullException("initiatorService");
+            if (userRepository == null) throw new ArgumentNullException("userRepository");
+
+            _initiatorService = initiatorService;
+            _userRepository = userRepository;
+        }
+
+        public void Handle(ChangeUserAddress command)
+        {
+            var initiator = _initiatorService.GetActiveById(command.InitiatorId);
+            var user = _userRepository.GetByGuid(command.UserId);
+
+            user.ChangeAddress(initiator, command.FirstName, command.LastName, command.Street, command.Postcode,
+                command.City, command.PhoneNumber);
+
+            _userRepository.Save(user);
+        }
+    }
+}
---
+++ tests/Phundus.Core.Tests/IdentityAccess/Application/ChangeUserAddressSpecs.cs
@@ -0,0 +1,39 @@
+﻿namespace Phundus.Tests.IdentityAccess.Application
+{
+    using Common.Domain.Model;
+    using developwithpassion.specifications.extensions;
+    using Machine.Specifications;
+    using Phundus.IdentityAccess.Application;
+    using Phundus.IdentityAccess.Users.Model;
+
+    [Subject(typeof (ChangeUserAddressHandler))]
+    public class when_handling_change_user_address :
+        identityaccess_command_handler_concern<ChangeUserAddress, ChangeUserAddressHandler>
+    {
+        private static string theFirstName = "The first name";
+        private static string theLastName = "The last name";
+        private static string theStreet = "The street";
+        private static string thePostcode = "The postcode";
+        private static string theCity = "The city";
+        private static string thePhoneNumber = "The phone number";
+        private static User theUser;
+
+        private Establish ctx = () =>
+        {
+            theUser = make.User();            
+
+            userRepository.setup(x => x.GetByGuid(theUser.UserId)).Return(theUser);
+
+            command = new ChangeUserAddress(theInitiatorId, theUser.UserId, theFirstName, theLastName, theStreet,
+                thePostcode, theCity, thePhoneNumber);
+        };
+
+        private It should_change_user_address = () =>
+            theUser.received(x =>
+                x.ChangeAddress(theInitiator, theFirstName, theLastName, theStreet, thePostcode, theCity,
+                    thePhoneNumber));
+
+        private It should_save_to_repository = () =>
+            userRepository.received(x => x.Save(theUser));
+    }
+}
