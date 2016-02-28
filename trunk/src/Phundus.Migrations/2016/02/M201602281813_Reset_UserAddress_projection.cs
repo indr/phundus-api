b9de8db03aa2441bba67a0eff1dfe9e4eda4b837
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2016-02-28 17:23:50 +0000

    Generate missing UserSignedUp events.

---
+++ trunk/src/Phundus.Migrations/2016/02/M201602281813_Reset_UserAddress_projection.cs
@@ -0,0 +1,20 @@
+namespace Phundus.Migrations
+{
+    using System;
+    using FluentMigrator;
+
+    [Migration(201602281813)]
+    public class M201602281813_Reset_UserAddress_projection : MigrationBase
+    {
+        public override void Up()
+        {
+            DeleteTable("Es_IdentityAccess_UserAddress");
+            ResetTracker("UserAddressProjection");
+        }
+
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
