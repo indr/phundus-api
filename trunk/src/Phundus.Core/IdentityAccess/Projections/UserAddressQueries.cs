7b684224a58476235336852c13d2a56bcfc8821e
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2016-02-28 17:48:22 +0000

    Address as content object.

--- trunk/src/Phundus.Core/IdentityAccess/Projections/UserAddressQueries.cs
+++ trunk/src/Phundus.Core/IdentityAccess/Projections/UserAddressQueries.cs
@@ -1,28 +1,24 @@
 namespace Phundus.IdentityAccess.Projections
 {
     using System;
-    using Common;
     using Common.Domain.Model;
     using Cqrs;
     using NHibernate;
 
     public interface IUserAddressQueries
     {
-        UserAddressData GetById(InitiatorId initiatorId, Guid userId);
+        UserAddressData FindById(InitiatorId initiatorId, Guid userId);
     }
 
     public class UserAddressQueries : QueryBase<UserAddressData>, IUserAddressQueries
     {
-        public UserAddressData GetById(InitiatorId initiatorId, Guid userId)
+        public UserAddressData FindById(InitiatorId initiatorId, Guid userId)
         {
             var query = QueryOver();
             AuthFilter(query, initiatorId);
             query.Where(p => p.UserId == userId);
 
-            var result = query.SingleOrDefault();
-            if (result == null)
-                throw new NotFoundException("User address {0} not found.", userId);
-            return result;
+            return query.SingleOrDefault();
         }
 
         private static void AuthFilter(IQueryOver<UserAddressData, UserAddressData> query, InitiatorId initiatorId)
