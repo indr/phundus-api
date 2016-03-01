5c1a4c39dbab6b11c575e63023f4c15ca4551b87
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2016-03-01 06:53:38 +0000

    Added missing files.

---
+++ trunk/src/Phundus.Persistence/Shop/Projections/ShopItemDocumentDataMap.cs
@@ -0,0 +1,22 @@
+namespace Phundus.Persistence.Shop.Projections
+{
+    using FluentNHibernate.Mapping;
+    using Phundus.Shop.Projections;
+
+    public class ShopItemDocumentDataMap : ClassMap<ShopItemDocumentData>
+    {
+        public ShopItemDocumentDataMap()
+        {
+            SchemaAction.All();
+            Table("Es_Shop_Item_Documents");
+
+            Id(x => x.DataId).GeneratedBy.GuidComb();
+
+            References(x => x.ShopItem, "ArticleId").UniqueKey("ArticleId_FileName");
+            
+            Map(x => x.FileLength);
+            Map(x => x.FileName).UniqueKey("ArticleId_FileName");
+            Map(x => x.FileType);
+        }
+    }
+}
