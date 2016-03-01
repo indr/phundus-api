82f1df8cf3847f076d0144a9a3d865eb14fef1ba
Reto Inderbitzin <mail@indr.ch>
2016-03-01 06:53:38 +0000

    Added missing files.

---
+++ src/Phundus.Persistence/Shop/Projections/ShopItemImageDataMap.cs
@@ -0,0 +1,22 @@
+namespace Phundus.Persistence.Shop.Projections
+{
+    using FluentNHibernate.Mapping;
+    using Phundus.Shop.Projections;
+
+    public class ShopItemImageDataMap : ClassMap<ShopItemImageData>
+    {
+        public ShopItemImageDataMap()
+        {
+            SchemaAction.All();
+            Table("Es_Shop_Item_Images");
+
+            Id(x => x.DataId).GeneratedBy.GuidComb();
+            
+            References(x => x.ShopItem, "ArticleId").UniqueKey("ArticleId_FileName");
+                        
+            Map(x => x.FileLength);
+            Map(x => x.FileName).UniqueKey("ArticleId_FileName");
+            Map(x => x.FileType);
+        }
+    }
+}
