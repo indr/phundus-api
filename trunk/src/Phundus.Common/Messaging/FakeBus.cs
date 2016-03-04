2a1a119f5c7ebc49161162adf793a38f0d0a38f7
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2016-03-04 06:28:21 +0000

    Added FakeBusSpecs.

--- trunk/src/Phundus.Common/Messaging/FakeBus.cs
+++ trunk/src/Phundus.Common/Messaging/FakeBus.cs
@@ -2,16 +2,19 @@
 {
     using System;
     using System.Threading;
+    using Castle.Core.Logging;
     using Commanding;
 
     public class FakeBus : IBus
     {
         private readonly ICommandDispatcher _commandDispatcher;
+        private readonly ILogger _logger;
 
-        public FakeBus(ICommandDispatcher commandDispatcher)
+        public FakeBus(ILogger logger, ICommandDispatcher commandDispatcher)
         {
+            if (logger == null) throw new ArgumentNullException("logger");
             if (commandDispatcher == null) throw new ArgumentNullException("commandDispatcher");
-
+            _logger = logger;
             _commandDispatcher = commandDispatcher;
         }
 
@@ -19,10 +22,20 @@
         {
             if (message is ICommand)
             {
-                ThreadPool.QueueUserWorkItem(o => _commandDispatcher.Dispatch(message));
+                ThreadPool.QueueUserWorkItem(o =>
+                {
+                    try
+                    {
+                        _commandDispatcher.Dispatch(message);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.Fatal("Command dispatcher threw exception.", ex);
+                    }
+                });
                 return;
             }
-            
+
 
             throw new InvalidOperationException("Unknown message.");
         }
