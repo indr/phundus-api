3160b19cda9e3450f430df0deb73588eff63f197
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2016-03-04 11:44:25 +0000

    Notifications by FakeBus.

--- trunk/src/Phundus.Common/Messaging/FakeBus.cs
+++ trunk/src/Phundus.Common/Messaging/FakeBus.cs
@@ -3,41 +3,71 @@
     using System;
     using System.Threading;
     using Castle.Core.Logging;
+    using Castle.Windsor;
     using Commanding;
+    using Notifications;
 
     public class FakeBus : IBus
     {
-        private readonly ICommandDispatcher _commandDispatcher;
-        private readonly ILogger _logger;
+        private readonly IWindsorContainer _container;
 
-        public FakeBus(ILogger logger, ICommandDispatcher commandDispatcher)
+        public FakeBus(IWindsorContainer container)
         {
-            if (logger == null) throw new ArgumentNullException("logger");
-            if (commandDispatcher == null) throw new ArgumentNullException("commandDispatcher");
-            _logger = logger;
-            _commandDispatcher = commandDispatcher;
+            if (container == null) throw new ArgumentNullException("container");
+            _container = container;
         }
 
         public void Send<T>(T message)
         {
             if (message is ICommand)
             {
-                ThreadPool.QueueUserWorkItem(o =>
-                {
-                    try
-                    {
-                        _commandDispatcher.Dispatch(message);
-                    }
-                    catch (Exception ex)
-                    {
-                        _logger.Fatal("Command dispatcher threw exception.", ex);
-                    }
-                });
+                SendCommand(message);
+                return;
+            }
+            if (message is Notification)
+            {
+                SendNotification(message as Notification);
                 return;
             }
 
+            throw new InvalidOperationException("Unknown message " + message.GetType().FullName);
+        }
 
-            throw new InvalidOperationException("Unknown message.");
+        private void SendNotification(Notification message)
+        {
+            ThreadPool.QueueUserWorkItem(o =>
+            {
+                var consumers = _container.Resolve<INotificationConsumerFactory>().GetNotificationConsumers();
+                var logger = _container.Resolve<ILogger>();
+                foreach (var each in consumers)
+                {
+                    try
+                    {
+                        each.Handle(message);
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.Fatal("Notification consumer threw exception.", ex);
+                    }
+                }
+            });
+        }
+
+        private void SendCommand<T>(T message)
+        {
+            ThreadPool.QueueUserWorkItem(o =>
+            {
+                var handler = _container.Resolve<ICommandHandlerFactory>().GetHandlerForCommand(message);
+                var logger = _container.Resolve<ILogger>();
+                try
+                {
+                    handler.Handle(message);
+                }
+                catch (Exception ex)
+                {
+                    logger.Fatal("Command handler threw exception.", ex);
+                }
+            });
         }
     }
 }
---
+++ trunk/src/Phundus.Common/Notifications/NotificationPublisher.cs
@@ -0,0 +1,73 @@
+﻿namespace Phundus.Common.Notifications
+{
+    using System;
+    using System.Collections.Generic;
+    using Castle.Core.Internal;
+    using Domain.Model;
+    using Eventing;
+    using Messaging;
+
+    public interface INotificationPublisher
+    {
+        void PublishNotification(StoredEvent storedEvent, Func<StoredEvent, DomainEvent> deserializer);
+        void PublishNotification(IEnumerable<StoredEvent> storedEvents, Func<StoredEvent, DomainEvent> deserializer);
+    }
+
+    public class BusNotificationPublisher : INotificationPublisher
+    {
+        private readonly IBus _bus;        
+
+        public BusNotificationPublisher(IBus bus)
+        {
+            if (bus == null) throw new ArgumentNullException("bus");
+            _bus = bus;
+        }
+
+        public void PublishNotification(StoredEvent storedEvent, Func<StoredEvent, DomainEvent> deserializer)
+        {
+            var notification = new Notification(storedEvent.EventId, deserializer(storedEvent), storedEvent.Version);
+
+            _bus.Send(notification);
+        }
+
+        public void PublishNotification(IEnumerable<StoredEvent> storedEvents, Func<StoredEvent, DomainEvent> deserializer)
+        {
+            storedEvents.ForEach(each => PublishNotification(each,deserializer));
+        }
+    }
+
+    public class InThreadNotificationPublisher : INotificationPublisher
+    {
+        private readonly INotificationConsumerFactory _consumerFactory;
+
+        public InThreadNotificationPublisher(INotificationConsumerFactory consumerFactory)
+        {
+            if (consumerFactory == null) throw new ArgumentNullException("consumerFactory");
+            _consumerFactory = consumerFactory;
+        }
+
+        public void PublishNotification(StoredEvent storedEvent, Func<StoredEvent, DomainEvent> deserializer)
+        {
+            var notification = new Notification(storedEvent.EventId, deserializer(storedEvent),
+                storedEvent.Version);
+
+            PublishNotification(notification);
+        }
+
+        public void PublishNotification(IEnumerable<StoredEvent> storedEvents, Func<StoredEvent, DomainEvent> deserializer)
+        {
+            foreach (var each in storedEvents)
+                PublishNotification(each, deserializer);
+        }
+
+        private void PublishNotification(Notification notification)
+        {
+            var handlers = _consumerFactory.GetNotificationConsumers();
+
+            foreach (var each in handlers)
+            {
+                each.Handle(notification);
+            }
+        }
+    }
+}
