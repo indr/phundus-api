683c0e6392464b6d00644653c8390584d8a7e1dc
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2016-03-11 07:54:16 +0000

    Added MailingInstallerSpecs.

---
+++ trunk/tests/Phundus.Common.Tests/Mailing/Installers/MailingInstallerSpecs.cs
@@ -0,0 +1,16 @@
+﻿namespace Phundus.Common.Tests.Mailing.Installers
+{
+    using Common.Mailing;
+    using Common.Mailing.Installers;
+    using Machine.Specifications;
+
+    [Subject(typeof (MailingInstaller))]
+    public class MailingInstallerSpecs : windsor_installer_concern<MailingInstaller>
+    {
+        private It should_resolve_IMailGateway = () =>
+            resolve<IMailGateway>().ShouldBeOfExactType<MailGateway>();
+
+        private It should_resolve_IMessageFactory = () =>
+            resolve<IMessageFactory>().ShouldBeOfExactType<MessageFactory>();
+    }
+}
