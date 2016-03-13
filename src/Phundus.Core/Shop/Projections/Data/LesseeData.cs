d54446543c46c84d6ce57e22263e0d1f8ddb370f
Reto Inderbitzin <mail@indr.ch>
2016-03-13 00:36:49 +0000

    Auth restrictions on lessee queries.

--- src/Phundus.Core/Shop/Projections/Data/LesseeData.cs
+++ src/Phundus.Core/Shop/Projections/Data/LesseeData.cs
@@ -4,18 +4,18 @@
 
     public class LesseeData
     {
-        internal LesseeData(Guid lesseeId, string name, string address, string phoneNumber, string emailAddress)
+        internal LesseeData(Guid lesseeId, string name, string postalAddress, string phoneNumber, string emailAddress)
         {
             LesseeId = lesseeId;
             Name = name;
-            Address = address;
+            PostalAddress = postalAddress;
             PhoneNumber = phoneNumber;
             EmailAddress = emailAddress;
         }
 
         public Guid LesseeId { get; private set; }
         public string Name { get; private set; }
-        public string Address { get; private set; }
+        public string PostalAddress { get; private set; }
         public string PhoneNumber { get; private set; }
         public string EmailAddress { get; private set; }
     }
--- src/Phundus.Core/Shop/Projections/LesseeQueries.cs
+++ src/Phundus.Core/Shop/Projections/LesseeQueries.cs
@@ -2,12 +2,13 @@
 {
     using System;
     using Common;
+    using Common.Domain.Model;
     using Common.Querying;
     using Integration.IdentityAccess;
 
     public interface ILesseeQueries
     {
-        LesseeData GetById(Guid lesseeId);
+        LesseeData GetById(CurrentUserId currentUserId, Guid lesseeId);
     }
 
     public class LesseeQueries : QueryBase, ILesseeQueries
@@ -19,13 +20,16 @@
             _usersQueries = usersQueries;
         }
 
-        public LesseeData GetById(Guid lesseeId)
+        public LesseeData GetById(CurrentUserId currentUserId, Guid lesseeId)
         {
+            if (currentUserId.Id != lesseeId)
+                throw new NotFoundException("Lessee {0} not found.", lesseeId);
+
             var user = _usersQueries.FindById(lesseeId);
             if (user == null)
                 throw new NotFoundException("Lessee {0} not found", lesseeId);
 
-            return new LesseeData(user.UserId, user.FullName, user.Street + "\n" + user.Postcode + " " + user.City,
+            return new LesseeData(user.UserId, user.FullName, user.FullName + "\n" + user.Street + "\n" + user.Postcode + " " + user.City,
                 user.MobilePhone, user.EmailAddress);
         }
     }
