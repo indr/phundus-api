a89d7f0e7e5d0324c0a662fcdbde923b669f011f
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2016-03-17 05:50:47 +0000

    Fixes container initialization.

--- trunk/src/Phundus.Core/App_Start/CoreInstaller.cs
+++ trunk/src/Phundus.Core/App_Start/CoreInstaller.cs
@@ -5,6 +5,7 @@
     using Castle.MicroKernel.Registration;
     using Castle.MicroKernel.SubSystems.Configuration;
     using Castle.Windsor;
+    using Common.Infrastructure.Persistence;
     using Common.Projecting;
     using Common.Querying;
     using Inventory.Infrastructure;
@@ -57,6 +58,14 @@
 
             container.Register(Component.For<IReservationRepository>()
                 .ImplementedBy<NhReservationsBasedOnOrdersRepository>());
+
+            container.Register(Classes.FromThisAssembly()
+                .BasedOn(typeof(NhRepositoryBase<>))
+                .WithServiceAllInterfaces());
+
+            container.Register(Classes.FromThisAssembly()
+                .BasedOn<EventSourcedRepositoryBase>()
+                .WithServiceAllInterfaces());
         }
     }
 }
