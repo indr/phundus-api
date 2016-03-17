d06cf2090582514ea3399bab3fa9143dcaee19b0
indr <indr@0fb50134-772a-4728-8726-cc0728632084>
2016-03-17 01:53:32 +0000

    Fix removal.

--- trunk/tests/Phundus.Core.Tests/command_handler_concern.cs
+++ trunk/tests/Phundus.Core.Tests/command_handler_concern.cs
@@ -1,13 +1,8 @@
 namespace Phundus.Tests
 {
-    using System;
-    using System.Linq.Expressions;
-    using Authorization;
     using Common.Commanding;
     using Common.Domain.Model;
-    using Machine.Fakes;
     using Machine.Specifications;
-    using Rhino.Mocks;
 
     public abstract class command_handler_concern<TCommand, THandler> : concern<THandler>
         where THandler : class, IHandleCommand<TCommand> where TCommand : ICommand
@@ -16,14 +11,10 @@ namespace Phundus.Tests
         protected static Initiator theInitiator;
         protected static TCommand command;
 
-        protected static IAuthorize authorize;
-
         private Establish ctx = () =>
         {
             theInitiatorId = new InitiatorId();
             theInitiator = new Initiator(theInitiatorId, "initiator@test.phundus.ch", "The Initiator");
-
-            authorize = depends.on<IAuthorize>();
         };
 
         public Because of = () =>
@@ -34,20 +25,5 @@ namespace Phundus.Tests
             else
                 sut.Handle(command);
         };
-
-        protected static IMethodCallOccurrence enforceInitiatorTo<TAccessObject>() where TAccessObject : IAccessObject
-        {
-            return authorize.WasToldTo(x =>
-                x.Enforce(Arg<InitiatorId>.Is.Equal(theInitiatorId),
-                    Arg<TAccessObject>.Is.NotNull));
-        }
-
-        protected static IMethodCallOccurrence enforceInitiatorTo<TAccessObject>(
-            Expression<Predicate<TAccessObject>> accessObjectPredicate) where TAccessObject : IAccessObject
-        {
-            return authorize.WasToldTo(x =>
-                x.Enforce(Arg<InitiatorId>.Is.Equal(theInitiatorId),
-                    Arg<TAccessObject>.Matches(accessObjectPredicate)));
-        }
     }
 }
