0014fd0393c61872ae613c73e55a2d27f4c34fe4
Reto Inderbitzin <mail@indr.ch>
2016-03-18 11:35:59 +0000

    Fixes acceptance tests.

--- src/Phundus.Migrations/Profiles/ResetDatabaseProfile.cs
+++ src/Phundus.Migrations/Profiles/ResetDatabaseProfile.cs
@@ -28,9 +28,8 @@
             DropTable("Es_Shop_Item");
             DropTable("Es_Shop_Items_Popularity");
             DropTable("Es_Shop_Items");
-            DropTable("Es_Shop_ProductDetails_Files");
-            DropTable("Es_Shop_ProductDetails_Documents");
-            DropTable("Es_Shop_ProductDetails_Images");
+            DropTable("Es_Shop_ProductDetails_Document");
+            DropTable("Es_Shop_ProductDetails_Image");
             DropTable("Es_Shop_ProductDetails");
             DropTable("Es_Shop_ProductList_Popularity");
             DropTable("Es_Shop_ProductList");
