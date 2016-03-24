f10f25ba65049da690365b3ab7709f9f0200dab2
Reto Inderbitzin <mail@indr.ch>
2016-03-24 08:42:12 +0000

    Added TimePeriodLibrary and implemented QuantityPeriods.

---
+++ src/Phundus.Common/Domain.Model/QuantityPeriods.cs
@@ -0,0 +1,113 @@
+﻿namespace Phundus.Common.Domain.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using Itenso.TimePeriod;
+
+    public class QuantityPeriods
+    {
+        private readonly ITimePeriodCollection _tpc = new TimePeriodCollection();
+
+        public IList<QuantityPeriod> Intervals
+        {
+            get { return new ReadOnlyCollection<QuantityPeriod>(_tpc.Cast<QuantityPeriod>().ToList()); }
+        }
+
+        public void Add(QuantityPeriod p)
+        {
+            var intersections = _tpc.IntersectionPeriods(p).Cast<QuantityPeriod>().ToList();
+            if (intersections.Count == 0)
+            {
+                _tpc.Add(p);
+                return;
+            }
+            foreach (var i in intersections)
+            {
+                var r = p.GetRelation(i);
+                switch (r)
+                {
+                    case PeriodRelation.After:
+                        throw new NotImplementedException("After");
+                        break;
+                    case PeriodRelation.StartTouching:
+                        AddPeriod(p.Start, p.End, p.Quantity);
+                        break;
+                    case PeriodRelation.StartInside:
+                        AddPeriod(p.Start, i.End, p.Quantity + i.Quantity);
+                        AddPeriod(i.End, p.End, p.Quantity);
+                        i.ShrinkEndTo(p.Start);
+                        break;
+                    case PeriodRelation.InsideStartTouching:
+                        AddPeriod(p.Start, p.End, p.Quantity + i.Quantity);
+                        i.ShrinkStartTo(p.End);
+                        break;
+                    case PeriodRelation.EnclosingStartTouching:
+                        AddPeriod(i.End, p.End, p.Quantity);
+                        i.AddQuantity(p.Quantity);
+                        break;
+                    case PeriodRelation.Enclosing:
+                        AddPeriod(p.Start, i.Start, p.Quantity);
+                        AddPeriod(i.End, p.End, p.Quantity);
+                        i.AddQuantity(p.Quantity);
+                        break;
+                    case PeriodRelation.EnclosingEndTouching:
+                        AddPeriod(p.Start, i.Start, p.Quantity);
+                        i.AddQuantity(p.Quantity);
+                        break;
+                    case PeriodRelation.ExactMatch:
+                        i.AddQuantity(p.Quantity);
+                        break;
+                    case PeriodRelation.Inside:
+                        AddPeriod(i.Start, p.Start, i.Quantity);
+                        AddPeriod(p.Start, p.End, p.Quantity + i.Quantity);
+                        i.ShrinkStartTo(p.End);
+                        break;
+                    case PeriodRelation.InsideEndTouching:
+                        AddPeriod(p.Start, p.End, p.Quantity + i.Quantity);
+                        i.ShrinkEndTo(p.Start);
+                        break;
+                    case PeriodRelation.EndInside:
+                        AddPeriod(p.Start, i.Start, p.Quantity);
+                        AddPeriod(i.Start, p.End, p.Quantity + i.Quantity);
+                        i.ShrinkStartTo(p.End);
+                        break;
+                    case PeriodRelation.EndTouching:
+                        AddPeriod(p.Start, p.End, p.Quantity);
+                        break;
+                    case PeriodRelation.Before:
+                        throw new NotImplementedException("Before");
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        private void AddPeriod(DateTime start, DateTime end, int quantity)
+        {
+            var period = new QuantityPeriod(new Period(start, end), quantity);            
+            _tpc.Add(period);
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(String.Format("QuantityPeriods ({0}):", _tpc.Count));
+
+            foreach (var each in Intervals.OrderBy(p => p.Start).ThenBy(p => p.End))
+                sb.AppendLine(each.Start.ToString(CultureInfo.GetCultureInfo("DE-ch")) + " - " +
+                              each.End.ToString(CultureInfo.GetCultureInfo("DE-ch")) + " | " + each.Quantity);
+
+            return sb.ToString().Trim();
+        }
+
+        public void Add(DateTime startUtc, DateTime endUtc, int quantity)
+        {
+            Add(new QuantityPeriod(new Period(startUtc, endUtc), quantity));
+        }
+    }
+}
--- tests/Phundus.Common.Tests/Domain.Model/PeriodSpecs.cs
+++ tests/Phundus.Common.Tests/Domain.Model/PeriodSpecs.cs
@@ -2,9 +2,10 @@
 {
     using System;
     using Common.Domain.Model;
+    using developwithpassion.specifications.rhinomocks;
     using Machine.Specifications;
 
-    public class period_concern
+    public class period_concern : Observes
     {
         protected static DateTime theFromUtc;
         protected static DateTime theToUtc;
@@ -19,32 +20,48 @@
     }
 
     [Subject(typeof (Period))]
-    public class when_creating_a_period : period_concern
+    public class when_creating_a_period_with_to_date : period_concern
     {
-        private Because of = () => sut = new Period(theFromUtc, theToUtc);
+        private Because of = () =>
+            sut = new Period(theFromUtc, theToUtc);
 
-        private It should_have_from_utc = () => sut.FromUtc.ShouldEqual(theFromUtc);
+        private It should_have_from_utc = () =>
+            sut.FromUtc.ShouldEqual(theFromUtc);
 
-        private It should_have_to_utc = () => sut.ToUtc.ShouldEqual(theToUtc);
+        private It should_have_to_utc = () =>
+            sut.ToUtc.ShouldEqual(theToUtc);
+    }
+
+    [Subject(typeof (Period))]
+    public class when_creating_a_period_with_timespan : period_concern
+    {
+        private Because of = () =>
+            sut = new Period(theFromUtc, TimeSpan.FromDays(2));
+
+        private It should_have_from_utc = () =>
+            sut.FromUtc.ShouldEqual(theFromUtc);
+
+        private It should_have_to_utc = () =>
+            sut.ToUtc.ShouldEqual(theFromUtc.Add(TimeSpan.FromDays(2)));
     }
 
     [Subject(typeof (Period))]
     public class when_creating_a_period_with_smaller_from_than_to : period_concern
     {
-        private static Exception caught;
-
         private Establish ctx = () =>
         {
             theFromUtc = DateTime.Now.AddDays(1);
             theToUtc = DateTime.Now.AddDays(-1);
         };
 
-        private Because of = () => caught = Catch.Exception(() => sut = new Period(theFromUtc, theToUtc));
+        private Because of = () => spec.catch_exception(() =>
+            sut = new Period(theFromUtc, theToUtc));
 
-        private It should_throw_argument_exception = () => caught.ShouldBeOfExactType<ArgumentException>();
+        private It should_throw_argument_exception = () =>
+            spec.exception_thrown.ShouldBeOfExactType<ArgumentException>();
 
-        private It should_throw_exception_with_message =
-            () => caught.Message.ShouldEqual("The from date must be less or equal the to date.");
+        private It should_throw_exception_with_message = () =>
+            spec.exception_thrown.Message.ShouldEqual("The from date must be less or equal the to date.");
     }
 
     [Subject(typeof (Period))]
@@ -59,20 +76,48 @@
             theOtherPeriod = new Period(theFromUtc, theToUtc);
         };
 
-        private Because of = () => result = Equals(sut, theOtherPeriod);
+        private Because of = () =>
+            result = Equals(sut, theOtherPeriod);
 
-        private It should_be_equal = () => result.ShouldBeTrue();
+        private It should_be_equal = () =>
+            result.ShouldBeTrue();
     }
 
     [Subject(typeof (Period))]
     public class when_creating_from_now_with_days : period_concern
     {
         private static int days = 2;
-        private Because of = () => sut = Period.FromNow(days);
 
-        private It should_have_from_utc = () => sut.FromUtc.ShouldBeCloseTo(DateTime.UtcNow, TimeSpan.FromMinutes(1));
+        private Because of = () =>
+            sut = Period.FromNow(days);
 
-        private It should_have_to_utc =
-            () => sut.ToUtc.ShouldBeCloseTo(DateTime.UtcNow.AddDays(days), TimeSpan.FromMinutes(1));
+        private It should_have_from_utc = () =>
+            sut.FromUtc.ShouldBeCloseTo(DateTime.UtcNow, TimeSpan.FromMinutes(1));
+
+        private It should_have_to_utc = () =>
+            sut.ToUtc.ShouldBeCloseTo(DateTime.UtcNow.AddDays(days), TimeSpan.FromMinutes(1));
+    }
+
+    [Subject(typeof (Period))]
+    public class two_periods_not_intersecting : period_concern
+    {
+        private static Period result;
+
+        private Because of = () =>
+            result = Period.FromNow(2).Intersection(new Period(DateTime.UtcNow.AddDays(99), TimeSpan.FromDays(1)));
+
+        private It should_not_intersect_and_return_null = () =>
+            result.ShouldBeNull();
+    }
+
+    [Subject(typeof (Period))]
+    public class two_periods_intersecting_at_start : period_concern
+    {
+        private static Period result;
+
+        private Because of = () =>
+            result = Period.FromNow(2).Intersection(new Period(DateTime.UtcNow.AddDays(-2), TimeSpan.FromDays(1)));
+
+        private It should_not_intersect_and_return_null;
     }
 }
