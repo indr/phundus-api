1ed0ad8baa721eea87f4a4a856c9228a6f82a2e9
Reto Inderbitzin <mail@indr.ch>
2016-04-09 01:49:59 +0000

    Added TagsQuery and Tag controllers.

---
+++ src/Phundus.Core/Inventory/Application/TagsQueryService.cs
@@ -0,0 +1,19 @@
+﻿namespace Phundus.Inventory.Application
+{
+    using System.Collections.Generic;
+    using Common.Querying;
+    using Projections;
+
+    public interface ITagsQueryService
+    {
+        IList<TagData> Query();
+    }
+
+    public class TagsQueryService : QueryServiceBase<TagData>, ITagsQueryService
+    {
+        public IList<TagData> Query()
+        {
+            return QueryOver().OrderBy(x => x.Name).Asc.List();
+        }
+    }
+}
---
+++ src/Phundus.Core/Inventory/Resources/ProductsTagsController.cs
@@ -0,0 +1,37 @@
+﻿namespace Phundus.Inventory.Resources
+{
+    using System;
+    using System.Net.Http;
+    using Application;
+    using AttributeRouting;
+    using AttributeRouting.Web.Http;
+    using Common.Domain.Model;
+    using Common.Resources;
+    using Newtonsoft.Json;
+
+    [RoutePrefix("api/inventory/products/{productId}/tags")]
+    public class ProductsTagsController : ApiControllerBase
+    {
+        [POST("")]
+        public virtual HttpResponseMessage Post(Guid productId, PostRequestContent rq)
+        {
+            Dispatch(new TagProductCommand(CurrentUserId, new ArticleId(productId), rq.Name));
+
+            return NoContent();
+        }
+
+        [DELETE("{name}")]
+        public virtual HttpResponseMessage Delete(Guid productId, string name)
+        {
+            Dispatch(new UntagProductCommand(CurrentUserId, new ArticleId(productId), name));
+
+            return NoContent();
+        }
+
+        public class PostRequestContent
+        {
+            [JsonProperty("name")]
+            public string Name { get; set; }
+        }
+    }
+}
