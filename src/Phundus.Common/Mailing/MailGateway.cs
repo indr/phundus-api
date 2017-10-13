3a8d17b7d2acb925ecfb745a55b4d40a17daea9c
indr <mail@indr.ch>
2017-10-13 17:58:50 +0200

    Save messages to pickup directory, prevent sending to test.phundus.ch

--- src/Phundus.Common/Mailing/MailGateway.cs
+++ src/Phundus.Common/Mailing/MailGateway.cs
@@ -9,7 +9,14 @@
 
     public class MailGateway : IMailGateway
     {
+        private static string _defaultPickupDirectoryLocation;
+
         public ILogger Logger { get; set; }
+
+        public MailGateway()
+        {
+            _defaultPickupDirectoryLocation = MapPath(@"~\App_Data\Mails");
+        }
 
         public void Send(DateTime date, string recipients, string subject, string body)
         {
@@ -30,22 +37,23 @@
                 return;
             }
 
+            // In maintenance method, we remove all recipients except test.phundus.ch or phundus.ch
             if (Config.InMaintenance)
             {
-                RemoveRecipients(message, @"@(test\.)?phundus\.ch$");
+                RemoveUnallowedRecipients(message, @"@(test\.)?phundus\.ch$");
                 if (message.To.Count == 0)
                     return;
             }
             SaveToPickupDirectory(message);
 
-            // Emails to test.phundus.ch are only stored in pickup directory
-            RemoveRecipients(message, @"@test\.phundus\.ch$");
+            // Emails to @test.phundus.ch should not leave our system 
+            RemoveTestRecipients(message);
             if (message.To.Count == 0)
                 return;
             SendUsingSystemNetMailSettings(message);
         }
 
-        private void RemoveRecipients(MailMessage message, string regex)
+        private void RemoveUnallowedRecipients(MailMessage message, string regex)
         {
             for (var idx = message.To.Count - 1; idx >= 0; idx--)
             {
@@ -55,23 +63,32 @@
             }
         }
 
+        private void RemoveTestRecipients(MailMessage message)
+        {
+            for (var idx = message.To.Count - 1; idx >= 0; idx--)
+            {
+                var to = message.To[idx];
+                if (to.Address.EndsWith("@test.phundus.ch"))
+                    message.To.Remove(to);
+            }
+        }
+
         private static void SendUsingSystemNetMailSettings(MailMessage message)
         {
             var client = GetClient();
+            // If the configured pickup directory is the same with our default pickup directory
+            // we don't send, respectively save to message again
+            if (client.DeliveryMethod == SmtpDeliveryMethod.SpecifiedPickupDirectory
+                && client.PickupDirectoryLocation == _defaultPickupDirectoryLocation)
+                return;
             client.Send(message);
         }
 
         private static void SaveToPickupDirectory(MailMessage message)
         {
-            var defaultPickupDirectoryLocation = MapPath(@"~\App_Data\Mails");
-
-            // If the system.net.mail.settings is equal to our default pickup directory location, prevent sending / storing
-            if (GetClient().PickupDirectoryLocation == defaultPickupDirectoryLocation)
-                return;
-
             var client = new SmtpClient("", 0);
             client.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
-            client.PickupDirectoryLocation = defaultPickupDirectoryLocation;
+            client.PickupDirectoryLocation = _defaultPickupDirectoryLocation;
             // Pickup delivery method requires EnableSsl to be false
             client.EnableSsl = false;
             client.Send(message);
